c5f91e6d34a0edb53eec3fe06a185f1794ef523e
Lyakhova-Anastasia <nastya.lyahova595@gmail.com>
2025-10-11 12:16:55 +0300

    hm1 done

---
+++ Площадь трапеции.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+float find_trapezoid_area(float trapezoid_base1, float trapezoid_base2, float trapezoid_height)
+{
+    return (trapezoid_base1 + trapezoid_base2) * trapezoid_height / 2;
+}
+
+int main()
+{
+    float trapezoidBase1, trapezoidBase2, trapezoidHeight;
+    std::cout << "Enter bases of the trapezoid: ";
+    std::cin >> trapezoidBase1 >> trapezoidBase2;
+    std::cout << '\n' << "Enter the height of the trapezoid: ";
+    std::cin >> trapezoidHeight;
+    std::cout << '\n' << "The area of the trapezoid: " << find_trapezoid_area(trapezoidBase1, trapezoidBase2, trapezoidHeight);
+    return 0;
+}
