aefe18bbb1464fb43e5d9d2291db948cda27379c
Dmitriy Girak <rodnayasem@gmail.com>
2025-10-11 20:14:35 +0300

    done HW1

---
+++ sem1/GirakDA/HW1/HW1/HW1.cpp
@@ -0,0 +1,30 @@
+﻿//площадь квадрата//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float storona;
+	cout << "Введите сторону квадрата = ";
+	cin >> storona;
+	cout << "Площадь вашего квадрата равна = " << storona * storona << endl;
+}
+//площадь трапеции//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float h, a, b;
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите высоту = ";
+	cin >> h;
+	cout << "Введите первую сторону = ";
+	cin >> a;
+	cout << "Введите вторую сторону = ";
+	cin >> b;
+	cout << "Площадь трапеции равна = " << (0.5 * (a + b)) * h << endl;
+}
