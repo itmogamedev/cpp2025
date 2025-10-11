69524e384682bebdc95357955749bc3601c3d246
GarikBog <128406494+GarikBog@users.noreply.github.com>
2025-10-11 11:22:15 +0300

    Bolee krasivoe DZ

--- BogdanovIgor/PervoeDZ/DZ/main.cpp
+++ BogdanovIgor/PervoeDZ/DZ/main.cpp
@@ -1,15 +1,25 @@
 #include<iostream>
 
-double AreaOfSquare(float SideSize) {
+double AreaOfSquare(double SideSize) {
 	return SideSize * SideSize;
 }
 
-double AreaOfTrapezoid(float height, float base1,float base2) {
+double AreaOfTrapezoid(double height, double base1,double base2) {
 	return (base1+base2)/2 * height;
 }
 
 int main() {
-
-	std::cout << AreaOfTrapezoid(5,3,2);
+	setlocale(LC_ALL, "rus");
+	double squareSide, TrapDownSide, TrapTopSide, TrapHeight;
+	std::cout << "¬ведите сторону квадрата:";
+	std::cin >> squareSide;
+	std::cout << "ѕлощадь квадрата: " << AreaOfSquare(squareSide);
+	std::cout << "\n¬ведите верхнее основание тарапеции:";
+	std::cin >> TrapTopSide;
+	std::cout << "¬ведите нижнее основание трапеции:";
+	std::cin >> TrapDownSide;
+	std::cout << "¬ведите высоту трапеции:";
+	std::cin >> TrapHeight;
+	std::cout <<"ѕлощадь трапеции: " << AreaOfTrapezoid(TrapTopSide, TrapDownSide, TrapHeight);
 	return 0;
 }
