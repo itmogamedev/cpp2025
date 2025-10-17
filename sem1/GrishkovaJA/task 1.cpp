71a790a4e6fa75863d08275047ca709c922dcda1
UliaGrishkova <501138@niuitmo.ru>
2025-10-17 15:55:07 +0300

    mini hw 1 done

---
+++ sem1/GrishkovaJA/task 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+namespace geometry {
+double SquareArea(double side) { return side * side; }
+}  // namespace geometry
+
+int main() {
+  double length_side = 0.0;
+  while (true) {
+    std::cout << "Enter the side of the square: ";
+    std::cin >> length_side;
+
+    if (length_side > 0.0) {
+      break;
+    } else {
+      std::cerr << "The length of the square should be >=0 " << std::endl;
+    }
+  }
+
+  double area = geometry::SquareArea(length_side);
+  std::cout << "Square area: " << area << std::endl;
+  return 0;
+}
---
+++ sem1/GrishkovaJA/task 2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+namespace geometry {
+
+double Area(double base1, double base2, double height) {
+  return (base1 + base2) * height / 2.0;
+}
+
+}  // namespace geometry
+
+int main() {
+  double base1 = 0.0;
+  double base2 = 0.0;
+  double height = 0.0;
+
+  while (true) {
+    std::cout << "Enter first base: ";
+    std::cin >> base1;
+    std::cout << "Enter second base: ";
+    std::cin >> base2;
+    std::cout << "Enter height: ";
+    std::cin >> height;
+
+    if ((base1 > 0.0) && (base2 > 0.0) && (height > 0.0)) {
+      break;
+    } else {
+      std::cerr << "The length of the trapezoid should be >= 0" << std::endl;
+    }
+  }
+
+  double area = geometry::Area(base1, base2, height);
+  std::cout << "Area trapezoid: " << area << std::endl;
+
+  return 0;
+}
