9f08361491486058c0892ba52ea042a50dc7e4a6
MiFy <r.bakman@icloud.com>
2025-10-17 22:14:57 +0300

    mini HW1 done

---
+++ sem1/BakmanRD/mini_homework_1/minHW_1.1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a = 0;
+    std::cout << "please enter the side of the square : ";
+    std::cin >> a;
+    while (a < 0) {
+        std::cout << "please enter a positive side of the square, the previous one was less then zero : ";
+        std::cin >> a;
+    }
+    std::cout << "the area of the square is: " << a*a;
+
+}
