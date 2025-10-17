2de077ce6951a7bafc6e24a939391c134c7a5777
ires-cloud <reznikov@niuitmo.ru>
2025-10-17 19:52:33 +0300

    removed .vscode folder

--- sem1/Reznikov-Iaroslav/homework_2/HW2.cpp
+++ sem1/Reznikov-Iaroslav/homework_2/HW2.cpp
@@ -2,15 +2,15 @@
 #include <string>
 #include <cassert>
 
-std::string From10toN(std::string, int);
-std::string From10toN(int, int);
-void TheSyracuseHypothesis(int num);
+std::string from10toN(std::string, int);
+std::string from10toN(int, int);
+void theSyracuseHypothesis(int num);
 
 int main(){
     int numForHypothesis;
     std::cout << "Enter number to check the Syracuse hypothesis: ";
     std::cin >> numForHypothesis;
-    TheSyracuseHypothesis(numForHypothesis);
+    theSyracuseHypothesis(numForHypothesis);
 
     // for(int i = 0; i < 50; i++){
     //     std::cout << "10b: " << i << ", 8b: "<< From10toN(i, 8) << ", 16b: "<< From10toN(i, 16) <<std::endl;
@@ -18,7 +18,8 @@ int main(){
     return 0;
 }
 
-void TheSyracuseHypothesis(int num){
+void theSyracuseHypothesis(int num){
+    std::cout << num << "->";
     while(num > 1){
         if(num % 2 == 0){
             num = num / 2;
@@ -36,7 +37,7 @@ void TheSyracuseHypothesis(int num){
 
 }
 
-std::string From10toN(std::string snum10, int N){
+std::string from10toN(std::string snum10, int N){
     assert(N <= 16);
     std::string symbols[16] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
     int inum10 = std::stoi(snum10);
@@ -51,10 +52,10 @@ std::string From10toN(std::string snum10, int N){
 }
 
 
-std::string From10toN(int inum10, int N){
-    return From10toN(std::to_string(inum10), N);
+std::string from10toN(int inum10, int N){
+    return from10toN(std::to_string(inum10), N);
 }
 
-int ConvertStringToInt(std::string str){
+int convertStringToInt(std::string str){
     return std::stoi(str);
 }
