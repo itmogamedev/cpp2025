dab5405c7e41fbbbd69560c6609d1599e65a3e41
mixanikk <mishakarabaev28@gmail.com>
2025-10-18 01:13:55 +0300

    оформление кода дз 1 по гугл гайду

--- sem1/KarabaevMI/Homework_mini_1/DZ_mini_1.cpp
+++ sem1/KarabaevMI/Homework_mini_1/DZ_mini_1.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-float SquareZone(float s) 
-    {
-        return s * s;
-    }
-int main() 
-    {
-        setlocale(LC_ALL, "RU");
-        float s;
-        std::cout << "Add a side of square: ";
-        std::cin >> s;
-        std::cout << "Square area: " << SquareZone(s) << std::endl;
-        return 0;
-    }
+float SquareZone(float s) {
+    return s * s;
+}
+int main() {
+    setlocale(LC_ALL, "RU");
+    float s;
+    std::cout << "Add a side of square: ";
+    std::cin >> s;
+    std::cout << "Square area: " << SquareZone(s) << std::endl;
+    return 0;
+}
--- sem1/KarabaevMI/Homework_mini_1/DZ_mini_1_2.cpp
+++ sem1/KarabaevMI/Homework_mini_1/DZ_mini_1_2.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-float TrapeZone(float b1, float b2, float h) 
-    {
-        return ((b1 + b2) * h) / 2;
-    }
-int main() 
-    {
-        setlocale(LC_ALL, "RU");
-        float b1, b2, h;
-        std::cout <<"Enter the length of one of the sides of the trapezoid base: ";
-        std::cin >> b1;
-        std::cout <<"Enter the length of one more side of the trapezoid base: ";
-        std::cin >> b2;
-        std::cout <<"Enter the height of the trapezoid: ";
-        std::cin >> h;
-        std::cout <<"Trapezoid area: "<< TrapeZone(b1, b2, h) <<std::endl;
-        return 0;
-    }
+float TrapeZone(float b1, float b2, float h) {
+    return ((b1 + b2) * h) / 2;
+}
+int main() {
+    setlocale(LC_ALL, "RU");
+    float b1, b2, h;
+    std::cout <<"Enter the length of one of the sides of the trapezoid base: ";
+    std::cin >> b1;
+    std::cout <<"Enter the length of one more side of the trapezoid base: ";
+    std::cin >> b2;
+    std::cout <<"Enter the height of the trapezoid: ";
+    std::cin >> h;
+    std::cout <<"Trapezoid area: "<< TrapeZone(b1, b2, h) <<std::endl;
+    return 0;
+}
