bf8928eaf052bff755b6bfe9d0ef808ffa67e87c
kiriread <bulatmub1@gmail.com>
2025-10-17 21:14:57 +0300

    mini hm2 done

---
+++ sem1/MubarakshinBR/homework_2/mini_hm_4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+	int number;
+	std::cout << "Enter any natural number: ";
+	std::cin >> number;
+
+	while (number != 1) {
+		if (number % 2 == 0) {
+			number /= 2;
+			std::cout << number << " ";
+		}
+		else {
+			number = (number * 3 + 1) / 2;
+			std::cout << number << " ";
+		}
+	}
+	std::cout << "\n";
+	std::cout << "Syracuse's hypothesis is correct";
+}
