34a19ac1cc5a8986f69c10f8fe2973c08312401b
FastGamer21 <114305848+FastGamer21@users.noreply.github.com>
2025-10-18 00:42:29 +0300

    Исправил ошибку в коде
    Оказывается нужно было сделать формулу квадрата и трапеции, а не параллелограмма...

--- sem1/EfimovVA/homework_1/minihomework1.cpp
+++ sem1/EfimovVA/homework_1/minihomework1.cpp
@@ -11,22 +11,23 @@
 //    double area = getSquareArea();
 //    std::cout << "Square of square (lol): " << area << std::endl;
 //    return 0;
-}
 #include <iostream>
 
-double parallelogramArea() {
-    double base, height;
+double trapezoidArea() {
+    double base1, base2, height;
 
-    std::cout << "Write a side of paralelogram: ";
-    std::cin >> base;
-    std::cout << "Write a height of paralelogram: ";
+    std::cout << "Write first base of trapezoid: ";
+    std::cin >> base1;
+    std::cout << "Write second base of trapezoid: ";
+    std::cin >> base2;
+    std::cout << "Write height of trapezoid: ";
     std::cin >> height;
 
-    return base * height;
+    return (base1 + base2) * height / 2;
 }
 
 int main() {
-    double area = parallelogramArea();
-    std::cout << "Square of paralelograma: " << area << std::endl;
+    double area = trapezoidArea();
+    std::cout << "Square of trapezoid: " << area << std::endl;
     return 0;
 }
