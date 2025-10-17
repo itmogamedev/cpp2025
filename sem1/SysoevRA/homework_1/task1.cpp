98dc4b8b3fbbd11ae22f46d4ec64b8a3fdb2ff0f
Goodmornee <goodmorneeee@gmail.com>
2025-10-17 21:33:13 +0300

    minihw_1 done

---
+++ sem1/SysoevRA/homework_1/task1.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+int main() {
+    double n;
+    std::cin >> n;
+    if (n > 0) {
+        std::cout << n * n;
+    }
+    else {
+        std::cout << "write a number greater than 0" << std::endl;
+    }
+
+}
