45c1e26ad78d65080d2ba7437523f7f9117ab8a7
NirsonTheFirst <Beljaevnikita@gmail.com>
2025-10-18 01:51:30 +0500

    hw1 is done

---
+++ sem1/BelyaevNA/homework_1/task_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int up_side = 0;
+int dn_side = 0;
+int high = 0;
+float trapez = 0;
+float calc_S(int up_side, int dn_side, int high) {
+  return (up_side + dn_side) * high / 2.;
+}
+int main() {
+  std::cin >> up_side;
+  std::cin >> dn_side;
+  std::cin >> high;
+  trapez = calc_S(up_side, dn_side, high);
+  std::cout << trapez;
+}
