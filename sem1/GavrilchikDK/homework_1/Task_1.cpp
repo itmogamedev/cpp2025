5f87b36929c95dbbdf89b32946c56d1395aec82f
M1trix-21 <zxcmtrix@gmail.com>
2025-10-18 01:52:44 +0300

    Add homework_1 tasks 1 and 2 by GavrilchikDK

---
+++ sem1/GavrilchikDK/homework_1/Task_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    int side;
+    std:: cout <<"Enter the side of the square:";
+    std:: cin >> side; 
+    std:: cout <<"Answer :";std:: cout << side * side;
+return 0; 
+
+}  
