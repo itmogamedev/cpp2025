e7c42cb80552547710ea816f5f353a9f6a962dd4
kiriread <bulatmub1@gmail.com>
2025-10-17 20:04:18 +0300

    mini hm1 done

---
+++ sem1/MubarakshinBR/homework_1/mini_hm_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    float square_side;
+    std::cout << "Enter the side of the square: ";
+    std::cin >> square_side;
+    std::cout << "The area of a square is: " << square_side * square_side;
+}
