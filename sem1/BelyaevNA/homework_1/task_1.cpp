a054c588c898da4392573c4bb93be69da5d14bba
NirsonTheFirst <Beljaevnikita@gmail.com>
2025-10-18 00:43:28 +0500

    hw1 is done again

--- sem1/BelyaevNA/homework_1/task_1.cpp
+++ sem1/BelyaevNA/homework_1/task_1.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 int side = 0;
 int square = 0;
-int calc_square(int side) { return side * side; }
+int calc_Square(int side) { return side * side; }
 int main() {
   std::cin >> side;
-  square = calc_square(side);
+  square = calc_Square(side);
   std::cout << square;
 }
