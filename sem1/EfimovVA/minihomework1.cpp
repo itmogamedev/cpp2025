3dcfddd18137e81e63b2695d36698360060ec47d
FastGamer21 <114305848+FastGamer21@users.noreply.github.com>
2025-10-18 00:08:31 +0300

    Сделал первое минидз
    Добавил скрипт на с++, где находятся две функции для нахождения площади квадрата и параллелограмма

---
+++ sem1/EfimovVA/minihomework1.cpp
@@ -0,0 +1,34 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//double getSquareArea() {
+//    double side;
+//    cout << "Write a side of square: ";
+//    cin >> side;
+//    return side * side;
+//}
+//
+//int main() {
+//    double area = getSquareArea();
+//    cout << "Square of square (Lmao): " << area << endl;
+//    return 0;
+//}
+#include <iostream>
+using namespace std;
+
+double parallelogramArea() {
+    double base, height;
+
+    cout << "Write the side of paralelograma: ";
+    cin >> base;
+    cout << "Write the side of height: ";
+    cin >> height;
+
+    return base * height;
+}
+
+int main() {
+    double area = parallelogramArea();
+    cout << "Square of paralelograma: " << area << endl;
+    return 0;
+}
