8ced00696ffcc48aa4401c8a1f144925d24c4f06
JesusLover2 <quertyterminator@mail.ru>
2025-10-18 01:04:55 +0300

    Сделано мини дз №2

---
+++ sem1/PolyashenkoNI/MiniHW_2/mHWtask_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main (){
+
+    int natural = 0;
+
+    std::cout << "Enter your num: ";
+    
+    if (!(std::cin >> natural)){
+        std::cout << "You have been banned :(" << std::endl;
+        return 1;
+    }
+
+    if (natural <= 0){
+        std::cout << "Please, enter positive number"<< std::endl;
+        return 2;
+    }
+
+    while (natural != 1){
+        if (natural % 2 == 0){
+            natural /= 2;
+            std::cout << natural << std::endl;
+        }
+
+        else {
+            natural = (natural * 3 + 1) / 2; 
+            std::cout << natural << std::endl;
+        }
+    }
+
+    
+}
