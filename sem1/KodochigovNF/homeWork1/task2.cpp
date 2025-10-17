959ad4ae2424b8b56be41a6e6be93a2bd9e073e5
DMStudioss <nikita1705197928@gmail.com>
2025-10-17 20:24:03 +0300

    Добавлен файл task2.cpp для трапеции

---
+++ sem1/KodochigovNF/homeWork1/task2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <locale>
+
+double calculateTrapezoidArea(double base_a, double base_b, double height) {
+    return ((base_a + base_b) / 2.0) * height;
+}
+
+int main() {
+    std::setlocale(LC_ALL, "");
+
+    double base_a, base_b, height;
+    std::cout << "Введите основание A трапеции: ";
+    std::cin >> base_a;
+    std::cout << "Введите основание B трапеции: ";
+    std::cin >> base_b;
+    std::cout << "Введите высоту трапеции: ";
+    std::cin >> height;
+
+    std::cout << "Площадь трапеции = " << calculateTrapezoidArea(base_a, base_b, height) << std::endl;
+
+    return 0;
+}
