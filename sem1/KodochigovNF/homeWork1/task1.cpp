d90a781a1a4aedd88411b53679252be701508d8b
DMStudioss <nikita1705197928@gmail.com>
2025-10-17 18:59:05 +0300

    Минидомашка 1: площадь квадрата и трапеции

---
+++ sem1/KodochigovNF/homeWork1/task1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <locale>
+
+double calculateSquareArea(double side_lenght) {
+	return side_lenght * side_lenght;
+}
+
+double calculateTrapezoidArea(double base_a, double base_b, double height) {
+	return ((base_a + base_b) / 2.0) * height;
+}
+
+int main() {
+	std::setlocale(LC_ALL, "");
+
+	double side, base_a, base_b, height;
+
+    std::cout << "Введите сторону квадрата: ";
+    std::cin >> side;
+    std::cout << "Площадь квадрата = " << calculateSquareArea(side) << std::endl;
+
+    std::cout << "\nВведите основание A трапеции: ";
+    std::cin >> base_a;
+    std::cout << "Введите основание B трапеции: ";
+    std::cin >> base_b;
+    std::cout << "Введите высоту трапеции: ";
+    std::cin >> height;
+
+    std::cout << "Площадь трапеции = " << calculateTrapezoidArea(base_a, base_b, height) << std::endl;
+
+    return 0;
+}
