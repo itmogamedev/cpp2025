2624f8ff41e8af4bf3186ebb1e52aa40d31d358d
6789012 <silverlions888@gmail.com>
2025-10-18 11:07:52 +0300

    Возвращён .gitignore и переработан код
    Удалены суффиксы, убрана ненужная очистка cin (она осталась от моего старого решения после разбивки задания на файлы из одного большого main() с циклом), добавлен комментарий про std::format и C++20, применено форматирование в стиле Google с помощью clang-tools

--- sem1/AlekseenkoAA/homework_1/task_2.cpp
+++ sem1/AlekseenkoAA/homework_1/task_2.cpp
@@ -1,40 +1,35 @@
 // task_2.cpp : вычисляет площадь трапеции
+#include <cmath>
 #include <iostream>
+#include <limits>
+#include <locale>
 #include <string>
 #include <typeinfo>
-#include <locale>
-#include <cmath>
-#include <limits>
 
 double trapeciaSquare(double a, double b, double h) {
-    double area_square = ((a + b) * h) / 2;
-    return area_square;
+  double area_square = ((a + b) * h) / 2;
+  return area_square;
 }
 
 int main() {
-    // std::setlocale(LC_CTYPE, "Russian"); // На двух разных компах почему c++ работает по разному: один не выводит русский язык с setlocal(), а другой выводит русский только без setlocale()
-    double a;
-    double b;
-    double h;
-    std::cout << "Функция подсчёта площади трапеции!\n";
-    std::cout << "Введите длину нижней грани трапеции: ";
-    std::cin >> a;
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // очищает буфер cin
-    std::cout << "\nВведите длину верхней грани трапеции: ";
-    std::cin >> b;
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // очищает буфер cin
-    std::cout << "\nВведите длину высоты трапеции: ";
-    std::cin >> h;
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // очищает буфер cin
-    try {
-        std::cout << "\nПлощадь трапеции: " << std::to_string(trapeciaSquare(a, b, h)) + "\n"; // to_string переводит переменную double в string тип
-    }
-    catch (std::exception ex) {
-        std::cout << "Что то пошло не так(!\n" << ex.what();
-        return -1;
-    }
-    return 0;
+  // std::setlocale(LC_CTYPE, "Russian"); // На двух разных компах почему c++
+  // работает по разному: один не выводит русский язык с setlocal(), а другой
+  // выводит русский только без setlocale()
+  double a;
+  double b;
+  double h;
+  std::cout << "Функция подсчёта площади трапеции!\n";
+  std::cout << "Введите длину нижней грани трапеции: ";
+  std::cin >> a;
+  std::cout << "\nВведите длину верхней грани трапеции: ";
+  std::cin >> b;
+  std::cout << "\nВведите длину высоты трапеции: ";
+  std::cin >> h;
+  try {
+    std::cout << "\nПлощадь трапеции: " << std::to_string(trapeciaSquare(a, b, h)) + "\n";  // to_string переводит переменную double в string тип
+  } catch (std::exception ex) {
+    std::cout << "Что то пошло не так(!\n" << ex.what();
+    return -1;
+  }
+  return 0;
 }
--- sem1/AlekseenkoAA/homework_2/task_1.cpp
+++ sem1/AlekseenkoAA/homework_2/task_1.cpp
@@ -1,28 +1,24 @@
-// task_1.cpp : преобразование из десятичной в шестнадцатеричную и восьмеричную систему счисления
+// task_1.cpp : преобразование из десятичной в шестнадцатеричную и восьмеричную
+// систему счисления
+#include <format> // ВНИМАНИЕ!!! Компиляция происходит для c++ 20+
 #include <iostream>
-#include <format>
-#include <string>
 #include <limits>
+#include <string>
 
-std::string dec2Hex(int a) {
-    return std::format("{:0x}", a);
-}
+std::string dec2Hex(int a) { return std::format("{:0x}", a); }
 
-std::string dec2Oct(int a) {
-    return std::format("{:o}", a);
-}
+std::string dec2Oct(int a) { return std::format("{:o}", a); }
 
-int main()
-{
-    // std::setlocale(LC_CTYPE, "Russian"); // На двух разных компах почему c++ работает по разному: один не выводит русский язык с setlocal(), а другой выводит русский только без setlocale()
-    /* Программа перевода из система в систему */
-    std::cout << "Тест функций перевода числе в разные системы счисления \n";
-    int sample_a;
-    std::cout << "Число в десятичной системе счисление: ";
-    std::cin >> sample_a;
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << "Число в шестнадцатеричной системе счисление:" << dec2Hex(sample_a) << "\n";
-    std::cout << "Число в восмиричной системе счисление:" << dec2Oct(sample_a) << "\n";
-    return 0;
+int main() {
+  // std::setlocale(LC_CTYPE, "Russian"); // На двух разных компах почему c++
+  // работает по разному: один не выводит русский язык с setlocal(), а другой
+  // выводит русский только без setlocale()
+  /* Программа перевода из система в систему */
+  std::cout << "Тест функций перевода числе в разные системы счисления \n";
+  int sample_a;
+  std::cout << "Число в десятичной системе счисление: ";
+  std::cin >> sample_a;
+  std::cout << "Число в шестнадцатеричной системе счисление:" << dec2Hex(sample_a) << "\n";
+  std::cout << "Число в восмиричной системе счисление:" << dec2Oct(sample_a) << "\n";
+  return 0;
 }
