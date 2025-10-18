b250cc080e47cacb08e88c6d99c3fcfa70c0fe37
tulakovtimur-sudo <tulakovtimur@gmail.com>
2025-10-18 14:25:54 +0300

    homework

---
+++ sem1/TulakovTR/homework_1/ConsoleApplication27.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	float a, b, h;
+	float s;
+	cout << "Введите числа" << endl;
+	cin >> a >> b >> h;
+	s = ((a + b) / 2) * h;
+	cout << "Площадь равна=" << s << endl;
+}
