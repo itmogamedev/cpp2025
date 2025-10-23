4686ab18d79f9577e52f42a176e93ad72c5f1eea
Ti-M-prog <apolosha.t@gmail.com>
2025-10-23 19:32:18 +0300

    Второе дз

---
+++ sem1/MagomedovTA/Homework 2/Convert.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+void to_16(int num) {
+  if (num == 0) return;
+  to_16(num / 16);
+
+  int ost = num % 16;
+  if (ost < 10)
+    std::cout << ost;
+  else
+    std::cout << char('A' + (ost - 10));
+}
+void to_8(int num) {
+  if (num == 0) return;
+  to_8(num / 8);
+  std::cout << (num % 8);
+}
+int main() {
+  int num;
+
+  std::cout << "Enter your num: ";
+  std::cin >> num;
+
+  std::cout << "Hex: ";
+  to_16(num);
+  std::cout << std::endl;
+  std::cout << "Oct: ";
+  to_8(num);
+
+  return 0;
+}
