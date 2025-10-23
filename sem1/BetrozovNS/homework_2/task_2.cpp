595dbd1d1e2d7608686d23550a5be47adf54a531
Niko Betrozov <inoplotfire@gmail.com>
2025-10-23 23:48:54 +0300

    Create task_2.cpp

---
+++ sem1/BetrozovNS/homework_2/task_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main() {
+  int nat = 0;
+
+  std::cout << "Начальное натуральное число: ";
+  std::cin >> nat;
+  
+  std::cout << "Маршрут: ";
+  while (nat != 1) {
+    std::cout << nat << " ";
+    if (nat % 2 == 0) {
+      nat /= 2;
+    }
+    else {
+      nat = (3 * nat + 1) / 2;
+    }
+  }
+  std::cout << std::endl;
+
+  std::cout << "Итог: 1, гипотеза верна :D" << std::endl;
+
+  return 0;
+}
+
