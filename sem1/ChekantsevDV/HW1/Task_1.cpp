bd703d0fbc5f2841faac201cb866c041bdfc8bb1
Agego14 <4ekan357@mail.ru>
2025-10-23 17:09:29 +0300

    HW1
    Сделал 2 задания первой мини домашки

---
+++ sem1/ChekantsevDV/HW1/Task_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	float a;
+
+	setlocale(LC_ALL, "Rus");
+	std::cout << "Введите сторону квадрата: ";
+	std::cin >> a;
+	if (a > 0)
+	{
+		std::cout << "Площадь квадрата равна: ";
+		std::cout << a * a;
+	}
+	else
+	{
+		std::cout << "Невозможная сторона квадрата";
+	}
+}
