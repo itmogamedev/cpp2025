50e243165e856f1c495df64130b2f5f5c6b4be1e
MiFy <r.bakman@icloud.com>
2025-10-23 20:45:07 +0300

    mini hw2 done

---
+++ sem1/BakmanRD/mini_homework_2/miniHW_2.1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+  int number;
+  cout << "please enter a number: ";
+  cin >> number;
+  cout << "the hexadecimal form of the number is " << hex << number << endl;
+  cout << "the octal form of the number is " << oct << number;
+
+  return 0;
+}
