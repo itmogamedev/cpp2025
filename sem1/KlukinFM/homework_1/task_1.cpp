7711d56ffdd85144e750d60f5f4cca7534d94f24
Fedor-Klukin <tobicot120@gmail.com>
2025-10-24 21:45:50 +0300

    Минидомашки 1 и 2

---
+++ sem1/KlukinFM/homework_1/task_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    float side;
+    std::cout<< "Введите сторону квадрата" << std:: endl;
+    std::cin >> side;
+    std::cout<< "Площадь квадрата равна " << side * side << std:: endl;
+
+    return 0;
+}
