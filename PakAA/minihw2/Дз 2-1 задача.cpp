1a346166855cca07459486fe69cd6e54065b7fe2
AndeWs <pak2007aa@gmail.com>
2025-10-24 17:34:49 +0300

    mini hw2 done

---
+++ PakAA/minihw2/Дз 2-1 задача.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+
+int main() {
+	int number;  
+
+	std::cout << "10 number: ";
+	std::cin >> number;
+	
+	std::cout << "16 number: " << std::hex << number << std::endl;
+
+	std::cout << std::dec;
+
+	std::cout << "8 number: " << std::oct << number << std::endl;
+
+	return 0;
+}
+
+
