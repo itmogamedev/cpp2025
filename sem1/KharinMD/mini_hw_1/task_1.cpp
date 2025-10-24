dd4806582d3a341fdc69b6d2e9af4367c41a7192
matveykharin <harinborzia@gmail.com>
2025-10-24 23:12:47 +0300

    mini_hw_1 done

---
+++ sem1/KharinMD/mini_hw_1/task_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+double сalculateSquareArea(double side_length) {
+    return side_length * side_length;
+}
+
+int main() {
+    double side;
+    std::cout << "Введите длину стороны квадрата " << std::endl;
+    std::cin >> side;
+    double area = сalculateSquareArea(side);
+    std::cout << "Площадь квадрата со стороной " << side << " равна " << area << std::endl;
+    return 0;
+}
