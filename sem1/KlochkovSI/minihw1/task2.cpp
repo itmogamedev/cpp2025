597a10790a5e38a89ad2d86eec26b455eb2e615b
GitKrip <lostik2005@gmail.com>
2025-10-24 14:42:30 +0300

    вернул task2.cpp на своё место
    случайно поменял файл из первой домашки :(

--- sem1/KlochkovSI/minihw1/task2.cpp
+++ sem1/KlochkovSI/minihw1/task2.cpp
@@ -1,53 +1,16 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
-
-std::string DecToHex(int a) {
-  if (a == 0) {
-    return "0";
-  }
-
-  std::string hex_chars = "0123456789ABCDEF";
-  std::string hex_result = "";
-
-  while (a > 0) {
-    int ostatok = a % 16;
-    hex_result += hex_chars[ostatok];
-    a /= 16;
-  }
-
-  std::reverse(hex_result.begin(), hex_result.end());
-
-  return hex_result;
+int TrapezoidArea(int side_a, int side_b, int height) {
+  int tra_area = ((side_a + side_b) * height) / 2;
+  return tra_area;
 }
-
-std::string DecToOct(int b) {
-  if (b == 0) {
-    return "0";
-  }
-
-  std::string oct_chars = "01234567";
-  std::string oct_result = "";
-
-  while (b > 0) {
-    int ost = b % 8;
-    oct_result += oct_chars[ost];
-    b /= 8;
-  }
-
-  std::reverse(oct_result.begin(), oct_result.end());
-
-  return oct_result;
-}
-
 int main() {
-  int DecNumb = 0;
-  std::cout << "Enter a number: ";
-  std::cin >> DecNumb;
-  std::string hex_string = DecToHex(DecNumb);
-  std::string oct_string = DecToOct(DecNumb);
-  std::cout << "OCT: " << oct_string << std::endl;
-  std::cout << "HEX: " << hex_string << std::endl;
-
+  int side_a, side_b, height = 0;
+  std::cout << "Enter side_a lenght" << std::endl;
+  std::cin >> side_a;
+  std::cout << "Enter side_b lenght" << std::endl;
+  std::cin >> side_b;
+  std::cout << "Enter height lenght" << std::endl;
+  std::cin >> height;
+  std::cout << TrapezoidArea(side_a, side_b, height);
   return 0;
 }
