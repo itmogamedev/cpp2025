b7485360d9e474a0d1d7f14304d88a6623493aa7
Goodmornee <goodmorneeee@gmail.com>
2025-10-24 17:06:06 +0300

    mini hw2 done

---
+++ sem1/SysoevRA/homework_2/task1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cout << "write a number" << std::endl;
+    std::cin >> n;
+    std::cout << "octal number system: " << std::oct << n << std::endl;
+    std::cout << "hexadecimal number system: " << std::hex << n << std::endl;
+
+    return 0;
+}
