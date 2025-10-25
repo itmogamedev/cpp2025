b602ea9241d322f8bebca181a70a906df1fb4ce1
IskatelNik <bombur07@gmail.com>
2025-10-25 20:48:37 +0300

    Home work number 2.1
    Исправленная домашняя работа №1 и №2

--- sem1/MakarovNE/miniHW_1/task1.cpp
+++ sem1/MakarovNE/miniHW_1/task1.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 
 int main() {
-    int side = 0;
-    int result = 0;
+  int side = 0;
+  int result = 0;
 
-    while (true) {
-        std::cout << "Enter the length of the side of the square: " << std::endl;
-        std::cin >> side;
+  while (true) {
+    std::cout << "Enter the length of the side of the square: " << std::endl;
+    std::cin >> side;
 
-        if (side <= 0) {
-            std::cout << "There is no such square!" << std::endl;
-        }
-        else {
-            result = side * side;
-            std::cout << "Square area: " << result << std::endl;
-        }
-
-        std::cout << "---------------------------------------------" << std::endl;
+    if (side <= 0) {
+      std::cout << "There is no such square!" << std::endl;
+    } else {
+      result = side * side;
+      std::cout << "Square area: " << result << std::endl;
     }
+
+    std::cout << "---------------------------------------------" << std::endl;
+  }
 }
