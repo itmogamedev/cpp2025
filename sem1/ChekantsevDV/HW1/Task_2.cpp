20318ab6ee2e78b4390acb751c4b7c311c2e0014
Agego14 <4ekan357@mail.ru>
2025-10-25 21:36:47 +0300

    мини дз 1
    Выполнил 2 задания мини дз

--- sem1/ChekantsevDV/HW1/Task_2.cpp
+++ sem1/ChekantsevDV/HW1/Task_2.cpp
@@ -1,23 +1,18 @@
 ﻿#include <iostream>
 
-int main()
-{
-    float a, b, h;
-    setlocale(LC_ALL, "Rus");
-    std::cout << "Введите длину первого основания трапеции: ";
-    std::cin >> a;
-    std::cout << "Введите длину второго основания трапеции: ";
-    std::cin >> b;
-    std::cout << "Введите длину высоты трапеции: ";
-    std::cin >> h;
-    if (a * b * h > 0)
-    {
-        std::cout << "Площадь трапеции равна: " << (a + b) / 2 * h << std::endl;
-    }
-    else
-    {
-        std::cout << "Введена(ы) неверное(ые) значение(я)";
-    }
-}   
-
-    
+int main() {
+  float base1, base2, height;
+  setlocale(LC_ALL, "Rus");
+  std::cout << "Введите длину первого основания трапеции: ";
+  std::cin >> base1;
+  std::cout << "Введите длину второго основания трапеции: ";
+  std::cin >> base2;
+  std::cout << "Введите длину высоты трапеции: ";
+  std::cin >> height;
+  if (base1 * base2 * height > 0) {
+    std::cout << "Площадь трапеции равна: " << (base1 + base2) / 2 * height
+              << std::endl;
+  } else {
+    std::cout << "Введена(ы) неверное(ые) значение(я)";
+  }
+}
