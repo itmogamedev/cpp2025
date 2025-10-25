b7f339cd3b1891d57232ebc79acac64dab8d829b
Pronomuos <158984685+Elvor12@users.noreply.github.com>
2025-10-26 02:48:15 +0300

    indents

--- sem1/ReshetnikovEO/MiniHomework1/MiniSquare.cpp
+++ sem1/ReshetnikovEO/MiniHomework1/MiniSquare.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
-int square(int a) {
-    return a * a;
-}
+int square(int a) { return a * a; }
 
 int main() {
-    int square_area;
-    std::cout << "Please, type number for area of the square" << std::endl;
-    std::cin >> square_area;
-    std::cout << "Square area = " << square(square_area) << std::endl;
-    return 0;
+  int square_area;
+  std::cout << "Please, type number for area of the square" << std::endl;
+  std::cin >> square_area;
+  std::cout << "Square area = " << square(square_area) << std::endl;
+  return 0;
 }
