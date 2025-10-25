3ee69134adb5663b762b8aee29b7d7b392b966f1
Mist10X <art.oplachko@gmail.com>
2025-10-25 15:39:25 +0300

    внес правки

--- sem1/OplachkoAA/minihomework_2/Part 1/part1.cpp
+++ sem1/OplachkoAA/minihomework_2/Part 1/part1.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
-void iz10v16(int a){
-    std::cout << std::hex << a << std::endl; 
+
+void from_dec_in_hex(int a){
+  std::cout << std::hex << a << std::endl; 
 }
-void iz10v8(int b){
-    std::cout << std::oct << b << std::endl;
+
+void from_dec_in_oct(int b){
+  std::cout << std::oct << b << std::endl;
 }
+
 int main(){
-    int a;
-    int b;
-    std::cout << "Введите десятичное число для перевода в шестнадцатиричную: ";
-    std::cin >> a;
-    iz10v16(a);
-    std::cout << "Введите десятичное число для перевода в восьмиричную: ";
-    std::cin >> b;
-    iz10v8(b);
-    return 0;
+  int a;
+  int b;
+  std::cout << "Введите десятичное число для перевода в шестнадцатиричную: ";
+  std::cin >> a;
+  from_dec_in_hex(a);
+  std::cout << "Введите десятичное число для перевода в восьмиричную: ";
+  std::cin >> b;
+  from_dec_in_oct(b);
+  return 0;
 }
--- sem1/OplachkoAA/minihomework_2/Part 2/part2.cpp
+++ sem1/OplachkoAA/minihomework_2/Part 2/part2.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-int main(){
-    int num;
-    std::cin >> num;
-    while (num != 1){
-        if (num%2 == 0){
-            num = num / 2;
-        }
-        else{
-            num = ((num*3)+1)/2;
-        }
-    if (num == 1){
-        std::cout << "its work!" << std::endl;
-    }
 
+int main() {
+  int num;
+  std::cin >> num;
+  
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num = num / 2;
+    } else {
+      num = (num * 3 + 1) / 2;
     }
-    return 0;
+    
+    if (num == 1) {
+      std::cout << "It works!" << std::endl;
+    }
+  }
+  
+  return 0;
 }
