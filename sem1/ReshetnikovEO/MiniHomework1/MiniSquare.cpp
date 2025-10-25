43dccb1486187af89853daa0607f91b9d938522a
Egor Reshetnikov <elvorresh12@gmail.com>
2025-10-25 14:04:06 +0300

    minihw1 last rework

--- sem1/ReshetnikovEO/MiniHomework1/MiniSquare.cpp
+++ sem1/ReshetnikovEO/MiniHomework1/MiniSquare.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-using namespace std;
 
 int square(int a) {
     return a * a;
@@ -7,8 +6,8 @@ int square(int a) {
 
 int main() {
     int square_area;
-    cout << "Please, type number for area of the square" << endl;
-    cin >> square_area;
-    cout << "Square area = " << square(square_area) << endl;
+    std::cout << "Please, type number for area of the square" << std::endl;
+    std::cin >> square_area;
+    std::cout << "Square area = " << square(square_area) << std::endl;
     return 0;
 }
