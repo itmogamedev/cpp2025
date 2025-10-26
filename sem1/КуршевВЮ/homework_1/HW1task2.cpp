bf286f73d061502d206656529baa9b57200a7d5c
kurshevvladislav <fusionvlad@mail.ru>
2025-10-26 16:11:47 +0300

    ispravil dz1

--- sem1/КуршевВЮ/homework_1/HW1task2.cpp
+++ sem1/КуршевВЮ/homework_1/HW1task2.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
-float trapezoidArea(float base1length, float base2length, float height)
+float trapezoidArea(float base_1_length, float base_2_length, float height)
 {
-    return (base1length + base2length) * height / 2;
+  return (base_1_length + base_2_length) * height / 2;
 }
 
 int main() {
-    float base1length, base2length, height;
-    std::cout << "Enter the trapezoid first base length: ";
-    std::cin >> base1length;
-    std::cout << "Enter the trapezoid second base length: ";
-    std::cin >> base2length;
-    std::cout << "Enter the trapezoid height: ";
-    std::cin >> height;
-    std::cout << "Trapezoid area: " << trapezoidArea(base1length, base2length, height) << std::endl;
-    return 0;
+  float base_1_length, base_2_length, height;
+  std::cout << "Enter the trapezoid first base length: ";
+  std::cin >> base_1_length, 
+  std::cout << "Enter the trapezoid second base length: ";
+  std::cin >> base_2_length, 
+  std::cout << "Enter the trapezoid height: ";
+  std::cin >> height;
+  std::cout << "Trapezoid area: " << trapezoidArea(base_1_length, base_2_length, height) << std::endl;
+  return 0;
 }
