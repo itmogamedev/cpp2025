4649a6bb68a2fffe91e7a652c73b5534e664ee14
kurshevvladislav <fusionvlad@mail.ru>
2025-10-26 16:08:07 +0300

    ispravil dz2

--- sem1/КуршевВЮ/homework_2/HW2task1.cpp
+++ sem1/КуршевВЮ/homework_2/HW2task1.cpp
@@ -3,33 +3,33 @@
 
 void toEightAndSixteen(int num)
 {
-    const char alf[17] = { "0123456789ABCDEF" };
-    std::string num8{};
-    int temp = num;
-    
-    while (num > 0)
-    {
-        num8 = {alf[num % 8] + num8};
-        num /= 8;
-    }
-    std::cout << "8: " << num8 << std::endl;
-    
-    num = temp;
-    std::string num16{};
-    
-    while (num > 0)
-    {
-        num16 = {alf[num % 16] + num16};
-        num /= 16;
-    }
+  const char alf[17] = { "0123456789ABCDEF" };
+  std::string num_8{};
+  int temp = num;
 
-    std::cout << "16: " << num16;
+  while (num > 0)
+  {
+    num_8 = {alf[num % 8] + num_8};
+    num /= 8;
+  }
+  std::cout << "8: " << num_8 << std::endl;
+
+  num = temp;
+  std::string num_16{};
+
+  while (num > 0)
+  {
+    num_16 = {alf[num % 16] + num_16};
+    num /= 16;
+  }
+
+  std::cout << "16: " << num_16;
 }
 
 int main()
 {
-    int num;
-    std::cout << "Input number: ";
-    std::cin >> num;
-    toEightAndSixteen(num);
+  int num;
+  std::cout << "Input number: ";
+  std::cin >> num;
+  toEightAndSixteen(num);
 }
--- sem1/КуршевВЮ/homework_2/HW2task2.cpp
+++ sem1/КуршевВЮ/homework_2/HW2task2.cpp
@@ -2,28 +2,28 @@
 
 int main()
 {
-    int n;
-    std::cout << "Enter a natural number: ";
-    std::cin >> n;
-    
-    if (n < 1)
+  int n;
+  std::cout << "Enter a natural number: ";
+  std::cin >> n;
+
+  if (n < 1)
+  {
+    std::cout << "chel ti....";
+    return 0;
+  }
+
+  while (n != 1)
+  {
+    if (n % 2 == 0)
     {
-        std::cout << "chel ti....";
-        return 0;
+      n /= 2;
+      std::cout << n << std::endl;
     }
-    
-    while (n != 1)
+    else
     {
-        if (n % 2 == 0)
-        {
-            n /= 2;
-            std::cout << n << std::endl;
-        }
-        else
-        {
-            n = (n * 3 + 1) / 2;
-            std::cout << n << std::endl;
-        }
+      n = (n * 3 + 1) / 2;
+      std::cout << n << std::endl;
     }
-    std::cout << "It's 1.";
+  }
+  std::cout << "It's 1.";
 }
