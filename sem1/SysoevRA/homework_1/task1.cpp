056b7368563a06b01c45674f4c546952047d3535
Goodmornee <goodmorneeee@gmail.com>
2025-10-27 21:32:40 +0300

    hw done

--- sem1/SysoevRA/homework_1/task1.cpp
+++ sem1/SysoevRA/homework_1/task1.cpp
@@ -1,14 +1,11 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 
 int main() {
-    double n;
-    std::cin >> n;
-    if (n > 0) {
-        std::cout << n * n;
-    }
-    else {
-        std::cout << "write a number greater than 0" << std::endl;
-    }
-
+  double n;
+  std::cin >> n;
+  if (n > 0) {
+    std::cout << n * n;
+  } else {
+    std::cout << "write a number greater than 0" << std::endl;
+  }
 }
--- sem1/SysoevRA/homework_2/task1.cpp
+++ sem1/SysoevRA/homework_2/task1.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
-int main()
-{
-    int n;
-    std::cout << "write a number" << std::endl;
-    std::cin >> n;
-    std::cout << "octal number system: " << std::oct << n << std::endl;
-    std::cout << "hexadecimal number system: " << std::hex << n << std::endl;
+int main() {
+  int n;
+  std::cout << "write a number" << std::endl;
+  std::cin >> n;
+  std::cout << "octal number system: " << std::oct << n << std::endl;
+  std::cout << "hexadecimal number system: " << std::hex << n << std::endl;
 
-    return 0;
+  return 0;
 }
