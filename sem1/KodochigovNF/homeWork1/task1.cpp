2ff194c64f61c034cbe3f563029ff5528c9fc4c7
DMStudioss <nikita1705197928@gmail.com>
2025-11-01 21:03:25 +0300

    Исправлены отступы под Google C++ Style

--- sem1/KodochigovNF/homeWork1/task1.cpp
+++ sem1/KodochigovNF/homeWork1/task1.cpp
@@ -2,18 +2,18 @@
 #include <locale>
 
 double calculateSquareArea(double side_length) {
-    return side_length * side_length;
+  return side_length * side_length;
 }
 
 int main() {
-    setlocale(LC_ALL, "");  
+  setlocale(LC_ALL, "");
 
-    double side_length;
-    std::cout << "Введите сторону квадрата: ";
-    std::cin >> side_length;
+  double side_length;
+  std::cout << "Введите сторону квадрата: ";
+  std::cin >> side_length;
 
-    std::cout << "Площадь квадрата = " << calculateSquareArea(side_length)
-        << std::endl;
+  std::cout << "Площадь квадрата = " << calculateSquareArea(side_length)
+            << std::endl;
 
-    return 0;
+  return 0;
 }
--- sem1/KodochigovNF/homeWork1/task2.cpp
+++ sem1/KodochigovNF/homeWork1/task2.cpp
@@ -2,27 +2,27 @@
 #include <locale>
 
 double calculateTrapezoidArea(double base_a, double base_b, double height) {
-    return ((base_a + base_b) / 2.0) * height;
+  return ((base_a + base_b) / 2.0) * height;
 }
 
 int main() {
-    setlocale(LC_ALL, "");  
+  setlocale(LC_ALL, "");
 
-    double base_a;
-    double base_b;
-    double height;
+  double base_a;
+  double base_b;
+  double height;
 
-    std::cout << "Введите основание A трапеции: ";
-    std::cin >> base_a;
+  std::cout << "Введите основание A трапеции: ";
+  std::cin >> base_a;
 
-    std::cout << "Введите основание B трапеции: ";
-    std::cin >> base_b;
+  std::cout << "Введите основание B трапеции: ";
+  std::cin >> base_b;
 
-    std::cout << "Введите высоту трапеции: ";
-    std::cin >> height;
+  std::cout << "Введите высоту трапеции: ";
+  std::cin >> height;
 
-    std::cout << "Площадь трапеции = "
-        << calculateTrapezoidArea(base_a, base_b, height) << std::endl;
+  std::cout << "Площадь трапеции = "
+            << calculateTrapezoidArea(base_a, base_b, height) << std::endl;
 
-    return 0;
+  return 0;
 }
