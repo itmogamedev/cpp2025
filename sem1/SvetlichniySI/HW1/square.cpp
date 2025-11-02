65b2aae4737d4faf471498122d38a8d7c74b3cd5
CEMEH12123 <semensv42@gmail.com>
2025-11-02 16:51:17 +0300

    changed tab size

--- sem1/SvetlichniySI/HW1/square.cpp
+++ sem1/SvetlichniySI/HW1/square.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-int main()
-{
-    int ln,s;
-    std::cout<<"vvedite storonu kvadrata";
-    std::cin>>ln;
-    s=ln*ln;
-    std::cout<<s;
-    return 0;
+
+int main() {
+  int ln, s;
+  std::cout << "vvedite storonu kvadrata";
+  std::cin >> ln;
+  s = ln * ln;
+  std::cout << s;
+  return 0;
 }
--- sem1/SvetlichniySI/HW2/task4.cpp
+++ sem1/SvetlichniySI/HW2/task4.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
 int main() {
-    std::cout << "vvedite chislo" << std::endl;
-    int a;
-    std::cin >> a;
-    while (true) {
-        if (a < 0) {
-            std::cout << "vi ne pravi, vsego dobrogo" << std::endl;
-            return 1;
-        }
-        if (a == 1) {
-            std::cout << "chislo ravno 1" << std::endl;
-            break;
-        }
-        if (a % 2 == 0) {
-            std::cout << "chislo chetnoe: " << a << "/2 == " << a / 2 << std::endl;
-            a = a / 2;
-        } else {
-            std::cout << "chislo nechetnoe: " << a << "*3+1 == " << a * 3 + 1 << std::endl;
-            a = 3 * a + 1;
-        }
+  std::cout << "vvedite chislo" << std::endl;
+  int a;
+  std::cin >> a;
+  while (true) {
+    if (a < 0) {
+      std::cout << "vi ne pravi, vsego dobrogo" << std::endl;
+      return 1;
     }
-    return 0;
+    if (a == 1) {
+      std::cout << "chislo ravno 1" << std::endl;
+      break;
+    }
+    if (a % 2 == 0) {
+      std::cout << "chislo chetnoe: " << a << "/2 == " << a / 2 << std::endl;
+      a = a / 2;
+    } else {
+      std::cout << "chislo nechetnoe: " << a << "*3+1 == " << a * 3 + 1 << std::endl;
+      a = 3 * a + 1;
+    }
+  }
+  return 0;
 }
