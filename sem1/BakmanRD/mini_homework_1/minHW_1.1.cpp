ceb78e5e508ad7417c71730aa36036b1c802e377
MiFy <r.bakman@icloud.com>
2025-11-06 19:19:29 +0300

    mini hws fixed

--- sem1/BakmanRD/mini_homework_1/minHW_1.1.cpp
+++ sem1/BakmanRD/mini_homework_1/minHW_1.1.cpp
@@ -1,14 +1,13 @@
 ﻿#include <iostream>
 
-int main()
-{
-    int a = 0;
-    std::cout << "please enter the side of the square : ";
+int main() {
+  int a = 0;
+  std::cout << "please enter the side of the square : ";
+  std::cin >> a;
+  while (a < 0) {
+    std::cout << "please enter a positive side of the square, the previous one "
+                 "was less then zero : ";
     std::cin >> a;
-    while (a < 0) {
-        std::cout << "please enter a positive side of the square, the previous one was less then zero : ";
-        std::cin >> a;
-    }
-    std::cout << "the area of the square is: " << a*a;
-
+  }
+  std::cout << "the area of the square is: " << a * a;
 }
