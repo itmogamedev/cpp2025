208821ee01696ad1c0145757d3b9fa825eb41e3b
Speelway <speelway1803@gmail.com>
2025-11-07 00:07:53 +0300

    hw1 and hw2 remake

--- sem1/Reus_AU/minihw_1/square_area.cpp
+++ sem1/Reus_AU/minihw_1/square_area.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 float SquareArea(float side) {
-	return side * side;
+  return side * side;
 }
 
 int main() {
 
-	float side;
-	std::cout << "Enter the length of the side of the square: ";
-	std::cin >> side;
-	std::cout << "Square area: " << SquareArea(side) << std::endl;
+  float side;
+  std::cout << "Enter the length of the side of the square: ";
+  std::cin >> side;
+  std::cout << "Square area: " << SquareArea(side) << std::endl;
 
-	return 0;
+  return 0;
 }
--- sem1/Reus_AU/minihw_2/Syracuse.cpp
+++ sem1/Reus_AU/minihw_2/Syracuse.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
 void syracuseHypothesis(unsigned long long num) {
-    while (num != 1) {
-        std::cout << num << " ";
-        if (num % 2 == 0) {
-            num = num / 2;
-        }
-        else {
-            num = (3 * num + 1) / 2;
-        }
+  while (num != 1) {
+    std::cout << num << " ";
+    if (num % 2 == 0) {
+      num = num / 2;
+    }
+    else {
+      num = (3 * num + 1) / 2;
+    }
     }
     std::cout << num << std::endl;
 }
 
 int main() {
-    unsigned long long num;
-    std::cout << "Enter number: ";
-    std::cin >> num;
-    syracuseHypothesis(num);
-    return 0;
+  unsigned long long num;
+  std::cout << "Enter number: ";
+  std::cin >> num;
+  syracuseHypothesis(num);
+  return 0;
 }
