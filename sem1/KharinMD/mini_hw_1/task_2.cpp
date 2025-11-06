502c0b08161da762dcb4ab3b117152dc208d4bb2
matveykharin <harinborzia@gmail.com>
2025-11-06 12:21:28 +0300

    Правки
    Внес необходимые корректировки

--- sem1/KharinMD/mini_hw_1/task_2.cpp
+++ sem1/KharinMD/mini_hw_1/task_2.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
 double сalculateTrapezoidArea(double base_1, double base_2, double height) {
-    return ((base_1 + base_2) / 2) * height;
+  return ((base_1 + base_2) / 2) * height;
 }
 
 int main() {
-    double base_a;
-    double base_b;
-    double height;
-    std::cout << "Введите длину основания A: ";
-    std::cin >> base_a;
+  double base_a;
+  double base_b;
+  double height;
+  std::cout << "Введите длину основания A: ";
+  std::cin >> base_a;
 
-    std::cout << "Введите длину основания B: ";
-    std::cin >> base_b;
+  std::cout << "Введите длину основания B: ";
+  std::cin >> base_b;
 
-    std::cout << "Введите высоту трапеции: ";
-    std::cin >> height;
-    double area = сalculateTrapezoidArea(base_a, base_b, height);
-    std::cout << "Значение площади трапеции: " << area << std:: endl;
-    return 0;
+  std::cout << "Введите высоту трапеции: ";
+  std::cin >> height;
+  double area = сalculateTrapezoidArea(base_a, base_b, height);
+  std::cout << "Значение площади трапеции: " << area << std:: endl;
+  return 0;
 }
