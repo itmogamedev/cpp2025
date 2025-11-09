73a0794d8463ea47b08934c9f105a9e184dbb93f
JesusLover2 <quertyterminator@mail.ru>
2025-11-09 03:16:04 +0300

    Доработано МиниДЗ№2

--- sem1/PolyashenkoNI/MiniHW_2/mHWtask_2.cpp
+++ sem1/PolyashenkoNI/MiniHW_2/mHWtask_2.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 
 int main (){
+  long long int natural = 0;
 
-    int natural = 0;
+  std::cout << "Enter your num: ";
+  
+  if (!(std::cin >> natural)){
+    std::cout << "You have been banned :(" << std::endl;
+    return 1;
+  }
 
-    std::cout << "Enter your num: ";
-    
-    if (!(std::cin >> natural)){
-        std::cout << "You have been banned :(" << std::endl;
-        return 1;
+  if (natural <= 0){
+    std::cout << "Please, enter positive number"<< std::endl;
+    return 2;
+  }
+
+  while (natural != 1){
+    if (natural % 2 == 0){
+      natural /= 2;
+      std::cout << natural << std::endl;
     }
 
-    if (natural <= 0){
-        std::cout << "Please, enter positive number"<< std::endl;
-        return 2;
+    else {
+      natural = (natural * 3 + 1) / 2; 
+      std::cout << natural << std::endl;
     }
-
-    while (natural != 1){
-        if (natural % 2 == 0){
-            natural /= 2;
-            std::cout << natural << std::endl;
-        }
-
-        else {
-
-            natural = (natural * 3 + 1) / 2; 
-            std::cout << natural << std::endl;
-        }
-    }
-
-    
+  }
 }
