c3d9e326e98d2c50abc8c7fb0ebf615f9aa83cd8
AndeWs <pak2007aa@gmail.com>
2025-11-09 23:55:35 +0300

    Vse vrode

---
+++ sem1/PakAA/minihw2/DZ2.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+int main() {
+  unsigned long long n;
+
+  std::cout << "Enter N: ";
+  std::cin >> n;
+
+  std::cout << "Sequence: ";
+
+  while (n != 1) {
+    std::cout << n << " ";
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else {
+      n = (3 * n + 1) / 2;
+    }
+  }
+
+  std::cout << n << std::endl;
+  std::cout << "Hypothesis correct" << std::endl;
+
+  return 0;
+}
+// Даня топ
