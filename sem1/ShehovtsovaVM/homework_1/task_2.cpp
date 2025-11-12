f01d291bfba5019074329e1f9de1c9e9c8699987
Tapok <92251492+MRfrog1111@users.noreply.github.com>
2025-11-12 21:51:53 +0300

    Fixed_final

--- sem1/ShehovtsovaVM/homework_1/task_2.cpp
+++ sem1/ShehovtsovaVM/homework_1/task_2.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 float calculateTrapezoidArea(float a, float b, float h) {
-  return 0.5 * (a + b) * h;
+	return 0.5 * (a + b) * h;
 }
 
 int main()
 {
-  float a, b, c;
-  cin >> a >> b >> c;
-  cout << calculateTrapezoidArea(a);
+	float a, b, c;
+	std::cin >> a >> b >> c;
+	std::cout << calculateTrapezoidArea(a, b, c);
 }
--- sem1/ShehovtsovaVM/homework_2/task_2.cpp
+++ sem1/ShehovtsovaVM/homework_2/task_2.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 
 void chechHypothesis(int a) {
-  while (a != 1) {
-    if (a % 2 == 0) {
-      a /= 2;
+    while (a != 1) {
+        if (a % 2 == 0) {
+            a /= 2;
+        }
+        else {
+            a = (a * 3 + 1) / 2;
+        }
+        std::cout << a << std::endl;
     }
-    else {
-      a = (a * 3 + 1) / 2;
-    }
-    cout << a << endl;
-  }
-  cout << "hypothesis is right!";
+    std::cout << "hypothesis is right!";
 }
 int main()
 {
-  int a;
-  cin >> a;
-  chechHypothesis(a);
-  return 0;
+    int a;
+    std::cin >> a;
+    chechHypothesis(a);
+    return 0;
 }
