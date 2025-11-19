9ba609c5d91cd846b457d9988d41330a2fccc42c
Alexa96-gg <alexadurova96969@gmail.com>
2025-11-19 21:42:41 +0300

    2 hw attached

---
+++ sem1/DurovaAV/MiniHw2/Hw 2.1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <sstream>
+
+int main()
+{
+    int num;
+    std::cout << "Enter a number ";
+    std::cin >> num;
+    std::cout << "Here is shestnadtsatirichnaya: " << std::hex << num << '\n';
+    std::cout << "Here is vosmirichnaya: " << std::oct << num << '\n';
+    return 0;
+}
---
+++ sem1/DurovaAV/MiniHw2/Hw 2.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    int num;
+    std::cout << "Enter a number ";
+    std::cin >> num;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        }
+        else {
+            num = (num * 3 + 1) / 2;
+        }
+        if (num == 1) {
+            std::cout << "It has become 1, as i said!";
+        }
+    }
+    return 0;
+}
