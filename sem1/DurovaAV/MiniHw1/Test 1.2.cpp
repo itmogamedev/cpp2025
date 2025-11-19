b8cc7b5e5d45ec959d232f64b6ee2e410f2ca1ea
Alexa96-gg <alexadurova96969@gmail.com>
2025-11-19 21:25:20 +0300

    done Hw1.2

---
+++ sem1/DurovaAV/MiniHw1/Test 1.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() 
+{
+    float base1, base2, height;
+    std::cout << "The first base of the trapezoid? ";
+    std::cin >> base1;
+    std::cout << "The second base of the trapezoid? ";
+    std::cin >> base2;
+    std::cout << "The height of the trapezoid? ";
+    std::cin >> height;
+    std::cout << "The area of the trapezoid is " << (base1 + base2) * height * 0.5;
+    return 0;
+}
