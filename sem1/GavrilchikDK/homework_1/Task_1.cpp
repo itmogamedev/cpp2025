3203fc32081688231eca67691c0c25a220fbedfa
M1trix-21 <zxcmtrix@gmail.com>
2025-11-29 00:35:51 +0300

    Фиксы для homework_1 по комментариям ревью

--- sem1/GavrilchikDK/homework_1/Task_1.cpp
+++ sem1/GavrilchikDK/homework_1/Task_1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-int main()
-{
-    int side;
-    std:: cout <<"Enter the side of the square:";
-    std:: cin >> side; 
-    std:: cout <<"Answer :";std:: cout << side * side;
-return 0; 
 
-}  
+int main() {
+  int side;
+  std::cout << "Enter the side of the square: ";
+  std::cin >> side;
+  std::cout << side * side;
+  return 0;
+}
--- sem1/GavrilchikDK/homework_1/Task_2.cpp
+++ sem1/GavrilchikDK/homework_1/Task_2.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 
 int main() {
-    int side_a;
-    std::cout << "Enter the side a: ";
-    std::cin >> side_a;
-
-    int side_b;
-    std::cout << "Enter the side b: ";
-    std::cin >> side_b;
-
-    int h;
-    std::cout << "Enter the height h: ";
-    std::cin >> h;
-
-    int area = (side_a + side_b) / 2 * h;
-    std::cout << "Answer: " << area << std::endl;
-
-    return 0;
+  int side_a;
+  std::cout << "Enter the side a: ";
+  std::cin >> side_a;
+  int side_b;
+  std::cout << "Enter the side b: ";
+  std::cin >> side_b;
+  int height;
+  std::cout << "Enter the height h: ";
+  std::cin >> height;
+  int area = (side_a + side_b) / 2 * height;
+  std::cout << "Answer: " << area << std::endl;
+  return 0;
 }
