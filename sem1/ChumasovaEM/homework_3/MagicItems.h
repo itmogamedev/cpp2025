9f13dd0439ab3010d6e123ac5936304b99999d56
Tushkan <risovashka001@gmail.com>
2025-11-29 00:37:50 +0300

    Добавлена HW3

---
+++ sem1/ChumasovaEM/homework_3/MagicItems.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <variant>
+#include <vector>
+#include <map>
+
+class MagicItem {
+public:
+    std::string name;
+    double price;
+    double weight;
+
+    MagicItem(const std::string& n, double p, double w) : name(n), price(p), weight(w) {}
+
+    virtual std::variant<int, double, std::string> getSpecificParameter() const = 0;
+    virtual std::string getType() const = 0;
+    virtual ~MagicItem() = default;
+};
+
+class Weapon;
+class Runes;
+class Potion;
+class Armor;
+class Clothes;
+class Scroll;
+
+
+struct shopStats {
+    std::string name;
+    int totalItems = 0;
+    double avgPrice = 0.0, avgWeight = 0.0;
+    std::map<std::string, double> typeAvgParams;
+    std::map<std::string, std::map<std::string, int>> Effects;
+};
+
+std::vector<std::unique_ptr<MagicItem>> readShop(std::ifstream& file, std::string shopName);
+shopStats calculateStats(std::string shopName, const std::vector<std::unique_ptr<MagicItem>>& items);
+void printStats(const shopStats& stats);
+void processShops();
