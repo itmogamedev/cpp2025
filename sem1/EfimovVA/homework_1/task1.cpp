4355c0b3ce07a0149d72343501cc5d107511991f
FastGamer21 <114305848+FastGamer21@users.noreply.github.com>
2025-11-29 11:28:03 +0300

    Учёл фидбэк
    Исправил косяк с двумя пробелами

--- sem1/EfimovVA/homework_1/task1.cpp
+++ sem1/EfimovVA/homework_1/task1.cpp
@@ -1,14 +1,14 @@
 ﻿#include <iostream>
 
 double getSquareArea() {
-    double side;
-    std::cout << "Write a side of square: ";
-    std::cin >> side;
-    return side * side;
+  double side;
+  std::cout << "Write a side of square: ";
+  std::cin >> side;
+  return side * side;
 }
 
 int main() {
-    double area = getSquareArea();
-    std::cout << "Square of square (lol): " << area << std::endl;
-    return 0;
+  double area = getSquareArea();
+  std::cout << "Square of square (lol): " << area << std::endl;
+  return 0;
 }
--- sem1/EfimovVA/homework_1/task2.cpp
+++ sem1/EfimovVA/homework_1/task2.cpp
@@ -1,20 +1,20 @@
 ﻿#include <iostream>
 
 double trapezoidArea() {
-    double base1, base2, height;
+  double base1, base2, height;
 
-    std::cout << "Write first base of trapezoid: ";
-    std::cin >> base1;
-    std::cout << "Write second base of trapezoid: ";
-    std::cin >> base2;
-    std::cout << "Write height of trapezoid: ";
-    std::cin >> height;
+  std::cout << "Write first base of trapezoid: ";
+  std::cin >> base1;
+  std::cout << "Write second base of trapezoid: ";
+  std::cin >> base2;
+  std::cout << "Write height of trapezoid: ";
+  std::cin >> height;
 
-    return (base1 + base2) * height / 2;
+  return (base1 + base2) * height / 2;
 }
 
 int main() {
-    double area = trapezoidArea();
-    std::cout << "Square of trapezoid: " << area << std::endl;
-    return 0;
+  double area = trapezoidArea();
+  std::cout << "Square of trapezoid: " << area << std::endl;
+  return 0;
 }
