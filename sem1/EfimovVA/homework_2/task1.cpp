3bd6e3ad86b79222c75c114ac6176fdd8fc6f490
FastGamer21 <114305848+FastGamer21@users.noreply.github.com>
2025-11-29 11:47:02 +0300

    Исправил оформление
    Отформатировал код с соответствии Google Style Guide

--- sem1/EfimovVA/homework_2/task1.cpp
+++ sem1/EfimovVA/homework_2/task1.cpp
@@ -2,43 +2,43 @@
 #include <string>
 
 std::string to_hex(int n) {
-    if (n == 0) return "0";
+  if (n == 0) return "0";
 
-    std::string alphabet = "0123456789ABCDEF";
-    std::string result = "";
-    int num = n;
+  std::string alphabet = "0123456789ABCDEF";
+  std::string result = "";
+  int num = n;
 
-    while (num > 0) {
-        int ostatok = num % 16;
-        result = alphabet[ostatok] + result;
-        num = num / 16;
-    }
+  while (num > 0) {
+    int ostatok = num % 16;
+    result = alphabet[ostatok] + result;
+    num = num / 16;
+  }
 
-    return result;
+  return result;
 }
 std::string to_octal(int n) {
-    if (n == 0) return "0";
+  if (n == 0) return "0";
 
-    std::string result = "";
-    int num = n;
+  std::string result = "";
+  int num = n;
 
-    while (num > 0) {
-        int ostatok = num % 8;
-        result = std::to_string(ostatok) + result;
-        num = num / 8;
-    }
+  while (num > 0) {
+    int ostatok = num % 8;
+    result = std::to_string(ostatok) + result;
+    num = num / 8;
+  }
 
-    return result;
+  return result;
 }
 int main() {
-    int number;
+  int number;
 
-    std::cout << "Write desitichnoe number: ";
-    std::cin >> number;
+  std::cout << "Write desitichnoe number: ";
+  std::cin >> number;
 
-    std::cout << "\nResults:" << std::endl;
-    std::cout << "desitichnoe: " << number << std::endl;
-    std::cout << "Sixteenarichnot: " << to_hex(number) << std::endl;
-    std::cout << "Vosmirichnoe: " << to_octal(number) << std::endl;
-    return 0;
+  std::cout << "\nResults:" << std::endl;
+  std::cout << "desitichnoe: " << number << std::endl;
+  std::cout << "Sixteenarichnot: " << to_hex(number) << std::endl;
+  std::cout << "Vosmirichnoe: " << to_octal(number) << std::endl;
+  return 0;
 }
--- sem1/EfimovVA/homework_2/task2.cpp
+++ sem1/EfimovVA/homework_2/task2.cpp
@@ -1,41 +1,36 @@
 ﻿#include <iostream>
 
 class peremena {
-public:
-    explicit peremena(int start_number)
-        : current_number(start_number) {
-    }
+ public:
+  explicit peremena(int start_number) : current_number(start_number) {}
 
-    void run() {
-        while (current_number != 1) {
-            std::cout << current_number << " -> ";
-            if (isEven(current_number)) {
-                current_number = current_number / 2;
-            }
-            else {
-                current_number = (current_number * 3 + 1) / 2;
-            }
-        }
-        std::cout << "1" << std::endl;
-        std::cout << "Hypothesis is proven" << std::endl;
+  void run() {
+    while (current_number != 1) {
+      std::cout << current_number << " -> ";
+      if (isEven(current_number)) {
+        current_number = current_number / 2;
+      } else {
+        current_number = (current_number * 3 + 1) / 2;
+      }
     }
+    std::cout << "1" << std::endl;
+    std::cout << "Hypothesis is proven" << std::endl;
+  }
 
-private:
-    int current_number;
+ private:
+  int current_number;
 
-    bool isEven(int number) {
-        return number % 2 == 0;
-    }
+  bool isEven(int number) { return number % 2 == 0; }
 };
 
 int main() {
-    int user_number;
+  int user_number;
 
-    std::cout << "Insert natural number ";
-    std::cin >> user_number;
+  std::cout << "Insert natural number ";
+  std::cin >> user_number;
 
-    peremena sequence(user_number);
-    sequence.run();
+  peremena sequence(user_number);
+  sequence.run();
 
-    return 0;
+  return 0;
 }
