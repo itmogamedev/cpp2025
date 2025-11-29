0fead3a885ad636d81d5b2a3d9a334f0a03d9e24
ssenpaio <nasnel.13.1@gmail.com>
2025-11-30 00:09:36 +0300

    дз1 сделано

---
+++ sem1/KutsaenkoNI/homework_1/task_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+double square_area(double side) { return side * side; }
+
+int main() {
+  double side;
+  setlocale(LC_ALL, "Russian");
+  std::cout << "Введите сторону квадрата: " << std::endl;
+  std::cin >> side;
+  if (side <= 0) {
+    std::cout << "Сторона квадрата не может быть отрицательной";
+    return 1;
+  }
+
+  std::cout << "Площадь квадрата: " << square_area(side) << std::endl;
+  return 0;
+}
---
+++ sem1/KutsaenkoNI/homework_1/task_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+double trapezoidArea(double a, double b, double h) { return (a + b) / 2 * h; }
+
+int main() {
+  double a, b, h;
+  setlocale(LC_ALL, "Russian");
+  std::cout << "Введите длину первого основания трапеции: ";
+  std::cin >> a;
+  std::cout << "Введите длину второго основания трапеции: ";
+  std::cin >> b;
+  std::cout << "Введите высоту трапеции: ";
+  std::cin >> h;
+
+  if (a < 0 || b < 0 || h < 0) {
+    std::cout << "Длины оснований и высота должны быть неотрицательными."
+              << std::endl;
+    return 1;
+  }
+
+  std::cout << "Площадь трапеции: " << trapezoidArea(a, b, h) << std::endl;
+  return 0;
+}
