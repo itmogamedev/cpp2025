9150f34f671687e58fc9258877a04226bfbe5a55
Alexa96-gg <alexadurova96969@gmail.com>
2025-11-29 12:02:58 +0300

    done hw 1, 2

--- sem1/DurovaAV/MiniHw2/Hw 2.1.cpp
+++ sem1/DurovaAV/MiniHw2/Hw 2.1.cpp
@@ -1,12 +1,11 @@
 ﻿#include <iostream>
 #include <sstream>
 
-int main()
-{
-    int num;
-    std::cout << "Enter a number ";
-    std::cin >> num;
-    std::cout << "Here is shestnadtsatirichnaya: " << std::hex << num << '\n';
-    std::cout << "Here is vosmirichnaya: " << std::oct << num << '\n';
-    return 0;
+int main() {
+  int num;
+  std::cout << "Enter a number ";
+  std::cin >> num;
+  std::cout << "Here is shestnadtsatirichnaya: " << std::hex << num << '\n';
+  std::cout << "Here is vosmirichnaya: " << std::oct << num << '\n';
+  return 0;
 }
--- sem1/DurovaAV/MiniHw2/Hw 2.2.cpp
+++ sem1/DurovaAV/MiniHw2/Hw 2.2.cpp
@@ -1,20 +1,18 @@
 ﻿#include <iostream>
 
-int main()
-{
-    int num;
-    std::cout << "Enter a number ";
-    std::cin >> num;
-    while (num != 1) {
-        if (num % 2 == 0) {
-            num = num / 2;
-        }
-        else {
-            num = (num * 3 + 1) / 2;
-        }
-        if (num == 1) {
-            std::cout << "It has become 1, as i said!";
-        }
+int main() {
+  int num;
+  std::cout << "Enter a number ";
+  std::cin >> num;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num = num / 2;
+    } else {
+      num = (num * 3 + 1) / 2;
     }
-    return 0;
+    if (num == 1) {
+      std::cout << "It has become 1, as I said!";
+    }
+  }
+  return 0;
 }
