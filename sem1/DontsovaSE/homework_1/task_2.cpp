c1dd41643be0b02d3fb1acd94eef22741ac9e4b4
sabinadontsova <sabina.dontsova@yandex.ru>
2025-11-29 19:49:51 +0400

    minihw1
    Done

---
+++ sem1/DontsovaSE/homework_1/task_2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+double calculateTrapezoidArea(double base_a, double base_b, double height) {
+    return (base_a + base_b) * height / 2.0;
+}
+
+int main() {
+    double base_a, base_b, height;
+    
+    std::cout << "Введите длину первого основания (a): ";
+    std::cin >> base_a;
+    if (std::cin.fail()) {
+        std::cout << "Ошибка: введите число!" << std::endl;
+        return 1;
+    }
+    
+    std::cout << "Введите длину второго основания (b): ";
+    std::cin >> base_b;
+    if (std::cin.fail()) {
+        std::cout << "Ошибка: введите число!" << std::endl;
+        return 1;
+    }
+    
+    std::cout << "Введите высоту трапеции (h): ";
+    std::cin >> height;
+    if (std::cin.fail()) {
+        std::cout << "Ошибка: введите число!" << std::endl;
+        return 1;
+    }
+    
+    if (base_a <= 0 || base_b <= 0 || height <= 0) {
+        std::cout << "Ошибка: все значения должны быть положительными!" << std::endl;
+        return 1;
+    }
+    
+    double area = calculateTrapezoidArea(base_a, base_b, height);
+    
+    std::cout << "Площадь трапеции: " << area << std::endl;
+    
+    return 0;
+}
