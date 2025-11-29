80b92a2683b44d3e06bc69f053ec7932184af901
MakariyNosierv <nosirevmakariy@gmail.com>
2025-11-29 20:18:48 +0300

    changet hw1 and did hw2

--- sem1/NosirevMK/Homework_1/Task_2.cpp
+++ sem1/NosirevMK/Homework_1/Task_2.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 int main()
 {
-	std::cout << "Enter first base:";
-	double a;
-	std::cin >> a;
-	std::cout << "Enter second base:";
-	double b;
-	std::cin >> b;
-	std::cout << "Enter heigh:";
-	double h;
-	std::cin >> h;
-	double S;
-	S = ((a + b) / 2) * h;
-	std::cout << "Area of trapezoid:" << S;
-	return 0;
+  std::cout << "Enter first base:";
+  double a;
+  std::cin >> a;
+  std::cout << "Enter second base:";
+  double b;
+  std::cin >> b;
+  std::cout << "Enter heigh:";
+  double h;
+  std::cin >> h;
+  double S;
+  S = ((a + b) / 2) * h;
+  std::cout << "Area of trapezoid:" << S;
+  return 0;
 }
