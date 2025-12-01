b3128925da9ff2cd9687108be660da75538021d2
Tushkan <risovashka001@gmail.com>
2025-12-01 17:47:08 +0300

    hw3 ...

--- sem1/ChumasovaEM/homework_3/MagicItems.h
+++ sem1/ChumasovaEM/homework_3/MagicItems.h
@@ -1,21 +1,23 @@
 #pragma once
+#include <map>
 #include <memory>
 #include <string>
 #include <variant>
 #include <vector>
-#include <map>
 
 class MagicItem {
-public:
-    std::string name;
-    double price;
-    double weight;
+ public:
+  std::string name;
+  double price;
+  double weight;
 
-    MagicItem(const std::string& n, double p, double w) : name(n), price(p), weight(w) {}
+  MagicItem(const std::string& n, double p, double w)
+      : name(n), price(p), weight(w) {}
 
-    virtual std::variant<int, double, std::string> getSpecificParameter() const = 0;
-    virtual std::string getType() const = 0;
-    virtual ~MagicItem() = default;
+  virtual std::variant<int, double, std::string> getSpecificParameter()
+      const = 0;
+  virtual std::string getType() const = 0;
+  virtual ~MagicItem() = default;
 };
 
 class Weapon;
@@ -25,16 +27,17 @@ class Armor;
 class Clothes;
 class Scroll;
 
-
 struct shopStats {
-    std::string name;
-    int totalItems = 0;
-    double avgPrice = 0.0, avgWeight = 0.0;
-    std::map<std::string, double> typeAvgParams;
-    std::map<std::string, std::map<std::string, int>> Effects;
+  std::string name;
+  int totalItems = 0;
+  double avgPrice = 0.0, avgWeight = 0.0;
+  std::map<std::string, double> typeAvgParams;
+  std::map<std::string, std::map<std::string, int>> Effects;
 };
 
-std::vector<std::unique_ptr<MagicItem>> readShop(std::ifstream& file, std::string shopName);
-shopStats calculateStats(std::string shopName, const std::vector<std::unique_ptr<MagicItem>>& items);
+std::vector<std::unique_ptr<MagicItem>> readShop(std::ifstream& file,
+                                                 std::string shopName);
+shopStats calculateStats(std::string shopName,
+                         const std::vector<std::unique_ptr<MagicItem>>& items);
 void printStats(const shopStats& stats);
 void processShops();
