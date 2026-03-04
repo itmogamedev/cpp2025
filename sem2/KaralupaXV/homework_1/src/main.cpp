e2d143e2ec08fe6bf53b5dca5be9348d8e6c0deb
Xiada <237393679+xiakarpa@users.noreply.github.com>
2026-03-04 17:59:59 +0300

    sem2-hw1

---
+++ sem2/KaralupaXV/homework_1/src/main.cpp
@@ -0,0 +1,16 @@
+#include "vec3.h"
+
+#include <iostream>
+
+
+int main() {
+	Vec3 a(1, 2, 4);
+	Vec3 b(2, 6, -2);
+	
+	Vec3 c = a.cross(b);
+	std::cout << c << "\nlen: " << c.len() << "\n";
+	
+	c = c.norm() * 10;
+	
+	std::cout << c << "\nlen: " << c.len() << "\n";
+}
