083612996d5d7af1161fb598259443e8aa77719b
OgnestrelYT <arssamohvalov@gmail.com>
2026-03-12 00:05:34 +0300

    fix gs

--- sem2/SamokhvalovAD/homework_1/dll/vec3.cpp
+++ sem2/SamokhvalovAD/homework_1/dll/vec3.cpp
@@ -1,34 +1,30 @@
 #include "vec3.h"
 
-
 Vec3 Vec3::sum(Vec3 other) {
-    return Vec3(x + other.x, y + other.y, z + other.z);
+  return Vec3(x + other.x, y + other.y, z + other.z);
 }
 
 Vec3 Vec3::minus(Vec3 other) {
-    return Vec3(x - other.x, y - other.y, z - other.z);
+  return Vec3(x - other.x, y - other.y, z - other.z);
 }
 
 Vec3 Vec3::scalarV(Vec3 other) {
-    return Vec3(x * other.x, y * other.y, z * other.z);
+  return Vec3(x * other.x, y * other.y, z * other.z);
 }
 
 Vec3 Vec3::scalar(double scalar) {
-    return Vec3(x * scalar, y * scalar, z * scalar);
+  return Vec3(x * scalar, y * scalar, z * scalar);
 }
 
 Vec3 Vec3::vec(Vec3 other) {
-    return Vec3(
-            y * other.z - z * other.y,
-            z * other.x - x * other.z,
-            x * other.y - y * other.x
-        );
+  return Vec3(y * other.z - z * other.y, z * other.x - x * other.z,
+              x * other.y - y * other.x);
 }
 
 Vec3 Vec3::norm() {
-    float length = this->length();
-    if (length == 0) {
-        return Vec3(0, 0, 0);
-    }
-    return Vec3(x / length, y / length, z / length);
+  float length = this->length();
+  if (length == 0) {
+    return Vec3(0, 0, 0);
+  }
+  return Vec3(x / length, y / length, z / length);
 }
--- sem2/SamokhvalovAD/homework_1/dll/vec3.h
+++ sem2/SamokhvalovAD/homework_1/dll/vec3.h
@@ -1,42 +1,42 @@
+#include <cmath>
 #include <iostream>
 #include <string>
-#include <cmath>
-
 
 class Vec3 {
-private:
-    double x, y, z;
-public:
-    Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
+ private:
+  double x, y, z;
 
-    double getX() { return x; }
-    double getY() { return y; }
-    double getZ() { return z; }
+ public:
+  Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
 
-    void setX(double x) { this->x = x; }
-    void setY(double y) { this->y = y; }
-    void setZ(double z) { this->z = z; }
+  double getX() { return x; }
+  double getY() { return y; }
+  double getZ() { return z; }
 
-    Vec3 sum(Vec3 other);
-    Vec3 operator+(const Vec3 other) { return sum(other); }
+  void setX(double x) { this->x = x; }
+  void setY(double y) { this->y = y; }
+  void setZ(double z) { this->z = z; }
 
-    Vec3 minus(Vec3 other);
-    Vec3 operator-(const Vec3 other) { return minus(other); }
+  Vec3 sum(Vec3 other);
+  Vec3 operator+(const Vec3 other) { return sum(other); }
 
-    Vec3 scalarV(Vec3 other);
-    Vec3 operator*(Vec3 other) { return scalarV(other); }
+  Vec3 minus(Vec3 other);
+  Vec3 operator-(const Vec3 other) { return minus(other); }
 
-    Vec3 scalar(double scalar);
+  Vec3 scalarV(Vec3 other);
+  Vec3 operator*(Vec3 other) { return scalarV(other); }
 
-    Vec3 vec(Vec3 other);
+  Vec3 scalar(double scalar);
 
-    Vec3 norm();
+  Vec3 vec(Vec3 other);
 
-    float length() { return sqrt(x * x + y * y + z * z); }
+  Vec3 norm();
 
-    friend std::ostream& operator<<(std::ostream& stream, Vec3& vec) {
+  float length() { return sqrt(x * x + y * y + z * z); }
+
+  friend std::ostream& operator<<(std::ostream& stream, Vec3& vec) {
     return stream << "(" << std::to_string(vec.x) << ", "
                   << std::to_string(vec.y) << ", " << std::to_string(vec.z)
                   << ")";
-    }
+  }
 };
