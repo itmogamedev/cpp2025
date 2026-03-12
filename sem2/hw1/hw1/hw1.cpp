cf2d153ff4e8505a14e9e89d92ffbe2518fcc2da
SaSBeaR122 <werty228sms@gmail.com>
2026-03-12 23:28:07 +0300

    Google Style

--- sem2/hw1/hw1/hw1.cpp
+++ sem2/hw1/hw1/hw1.cpp
@@ -1,23 +1,21 @@
-﻿#include "Vec3.h"
+﻿#include <iostream>
 
-#include <iostream>
 #include "Vec3.h"
 
-int main()
-{
-    Vec3 a(1, 2, 3);
-    Vec3 b(4, 5, 6);
+int main() {
+  Vec3 a(1, 2, 3);
+  Vec3 b(4, 5, 6);
 
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
+  std::cout << "a = " << a << std::endl;
+  std::cout << "b = " << b << std::endl;
 
-    std::cout << "a + b = " << a + b << std::endl;
-    std::cout << "a - b = " << a - b << std::endl;
-    std::cout << "a * 2 = " << a * 2 << std::endl;
+  std::cout << "a + b = " << a + b << std::endl;
+  std::cout << "a - b = " << a - b << std::endl;
+  std::cout << "a * 2 = " << a * 2 << std::endl;
 
-    std::cout << "dot(a,b) = " << a.dot(b) << std::endl;
-    std::cout << "cross(a,b) = " << a.cross(b) << std::endl;
+  std::cout << "dot(a,b) = " << a.dot(b) << std::endl;
+  std::cout << "cross(a,b) = " << a.cross(b) << std::endl;
 
-    std::cout << "length(a) = " << a.length() << std::endl;
-    std::cout << "normalize(a) = " << a.normalize() << std::endl;
+  std::cout << "length(a) = " << a.length() << std::endl;
+  std::cout << "normalize(a) = " << a.normalize() << std::endl;
 }
--- sem2/hw1Dll/hw1Dll/Vec3.h
+++ sem2/hw1Dll/hw1Dll/Vec3.h
@@ -8,22 +8,21 @@
 
 #include <iostream>
 
-struct HW1DLL_API Vec3
-{
-    float x, y, z;
+struct HW1DLL_API Vec3 {
+  float x, y, z;
 
-    Vec3();
-    Vec3(float x, float y, float z);
+  Vec3();
+  Vec3(float x, float y, float z);
 
-    Vec3 operator+(const Vec3& v) const;
-    Vec3 operator-(const Vec3& v) const;
-    Vec3 operator*(float s) const;
+  Vec3 operator+(const Vec3& v) const;
+  Vec3 operator-(const Vec3& v) const;
+  Vec3 operator*(float s) const;
 
-    float dot(const Vec3& v) const;
-    Vec3 cross(const Vec3& v) const;
+  float dot(const Vec3& v) const;
+  Vec3 cross(const Vec3& v) const;
 
-    float length() const;
-    Vec3 normalize() const;
+  float length() const;
+  Vec3 normalize() const;
 };
 
 HW1DLL_API std::ostream& operator<<(std::ostream& os, const Vec3& v);
