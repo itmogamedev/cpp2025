b56be67d7163dc7fdbf2c488fd2dd7828ba349cf
Kukarekun <pixel.bro.2017@bk.ru>
2026-03-13 19:16:55 +0300

    MiniHW1

---
+++ sem2/KokoevAA/Minihw1/vec3_library/vec3_library/Vec3.cpp
@@ -0,0 +1,61 @@
+#include "Vec3.h"
+
+#include <cmath>
+
+Vec3::Vec3() : x(0.0), y(0.0), z(0.0) {}
+
+Vec3::Vec3(double x_value, double y_value, double z_value)
+    : x(x_value), y(y_value), z(z_value) {
+}
+
+Vec3 Vec3::add(const Vec3& other) const {
+    return Vec3(x + other.x, y + other.y, z + other.z);
+}
+
+Vec3 Vec3::subtract(const Vec3& other) const {
+    return Vec3(x - other.x, y - other.y, z - other.z);
+}
+
+Vec3 Vec3::multiplyByScalar(double scalar) const {
+    return Vec3(x * scalar, y * scalar, z * scalar);
+}
+
+double Vec3::scalarMultiply(const Vec3& other) const {
+    return x * other.x + y * other.y + z * other.z;
+}
+
+Vec3 Vec3::vectorMultiply(const Vec3& other) const {
+    return Vec3(y * other.z - z * other.y, z * other.x - x * other.z,
+        x * other.y - y * other.x);
+}
+
+double Vec3::length() const {
+    return std::sqrt(x * x + y * y + z * z);
+}
+
+Vec3 Vec3::normalize() const {
+    double vec_length = length();
+
+    if (vec_length == 0.0) {
+        return Vec3(0.0, 0.0, 0.0);
+    }
+
+    return Vec3(x / vec_length, y / vec_length, z / vec_length);
+}
+
+Vec3 Vec3::operator+(const Vec3& other) const {
+    return add(other);
+}
+
+Vec3 Vec3::operator-(const Vec3& other) const {
+    return subtract(other);
+}
+
+Vec3 Vec3::operator*(double scalar) const {
+    return multiplyByScalar(scalar);
+}
+
+std::ostream& operator<<(std::ostream& out, const Vec3& vec) {
+    out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+    return out;
+}
---
+++ sem2/KokoevAA/Minihw1/vec3_library/vec3_library/Vec3.h
@@ -0,0 +1,38 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <iostream>
+
+#ifdef VEC3LIBRARY_EXPORTS
+#define VEC3_API __declspec(dllexport)
+#else
+#define VEC3_API __declspec(dllimport)
+#endif
+
+class VEC3_API Vec3 {
+public:
+	double x;
+	double y;
+	double z;
+
+	Vec3();
+	Vec3(double x_value, double y_value, double z_value);
+
+	Vec3 add(const Vec3& other) const;
+	Vec3 subtract(const Vec3& other) const;
+	Vec3 multiplyByScalar(double scalar) const;
+
+	double scalarMultiply(const Vec3& other) const;
+	Vec3 vectorMultiply(const Vec3& other) const;
+
+	double length() const;
+	Vec3 normalize() const;
+
+	Vec3 operator+(const Vec3& other) const;
+	Vec3 operator-(const Vec3& other) const;
+	Vec3 operator*(double scalar) const;
+};
+
+VEC3_API std::ostream& operator<<(std::ostream& out, const Vec3& vec);
+
+#endif
