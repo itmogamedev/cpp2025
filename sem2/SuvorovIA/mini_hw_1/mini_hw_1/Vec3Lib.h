a20b07b7e9bec78a00d019515fb32360fa9aee23
surpatov <privetivan26@gmail.com>
2026-03-13 21:10:29 +0300

    Мини д/з #1

---
+++ sem2/SuvorovIA/mini_hw_1/mini_hw_1/Vec3Lib.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifdef VEC3LIBRARY_EXPORTS
+#define VEC3LIB_API __declspec(dllexport)
+#else
+#define VEC3LIB_API __declspec(dllimport)
+#endif
+
+#include <iostream>
+
+struct VEC3LIB_API Vec3 {
+  float x;
+  float y;
+  float z;
+
+  Vec3();
+  Vec3(float x, float y, float z);
+  Vec3(const Vec3& other);
+  Vec3& operator=(const Vec3& other);
+  friend std::ostream& operator<<(std::ostream& os, const Vec3& v);
+
+  float length() const;
+  void normalize();
+  Vec3 normalized() const;
+  float dot(Vec3& other) const;
+  Vec3 cross(Vec3& other) const;
+};
+
+VEC3LIB_API Vec3 operator+(const Vec3& a, const Vec3& b);
+VEC3LIB_API Vec3 operator-(const Vec3& a, const Vec3& b);
+VEC3LIB_API Vec3 operator*(const Vec3& a, float value);
+VEC3LIB_API Vec3 operator*(float value, const Vec3& a);
