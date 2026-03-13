93e3564d564fabcb7f9a583f9bc9f649f9ecf604
MiFy <81895134+XDMiFy@users.noreply.github.com>
2026-03-13 14:30:01 +0300

    mini HomeWork1
    .dylib c 3д векторами для macOS :(

---
+++ sem2/BakmanRD/mathlib/vec3.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+
+
+#define MATHLIB
+
+struct MATHLIB Vec3 {
+    double x, y, z;
+
+    // Конструктор с параметрами по умолчанию
+    Vec3(double x = 0.0, double y = 0.0, double z = 0.0);
+};
+
+// Операторы
+MATHLIB Vec3 operator+(const Vec3& a, const Vec3& b);
+MATHLIB Vec3 operator-(const Vec3& a, const Vec3& b);
+MATHLIB Vec3 operator*(const Vec3& v, double scalar);
+MATHLIB std::ostream& operator<<(std::ostream& os, const Vec3& v);
+
+inline Vec3 operator*(double scalar, const Vec3& v) { return v * scalar; }
+
+MATHLIB double dot(const Vec3& a, const Vec3& b);
+MATHLIB Vec3   cross(const Vec3& a, const Vec3& b);
+MATHLIB double length(const Vec3& v);
+MATHLIB Vec3   normalize(const Vec3& v);
