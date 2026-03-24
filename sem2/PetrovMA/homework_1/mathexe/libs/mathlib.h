ca531107040fef5ea3a10239a840067f38c672ce
rosemeowry <rosemary.dev@proton.me>
2026-03-24 11:27:12 +0300

    fixed style guide

--- sem2/PetrovMA/homework_1/mathexe/libs/mathlib.h
+++ sem2/PetrovMA/homework_1/mathexe/libs/mathlib.h
@@ -1,21 +1,25 @@
-#ifndef MATHLIB_LIBRARY_H
-#define MATHLIB_LIBRARY_H
+#ifndef MATHLIB_LIBRARY_H_
+#define MATHLIB_LIBRARY_H_
+
 #include <iosfwd>
 
-struct Vec3 {
-    double x, y, z;
-    double length();
-    Vec3 normalize();
+namespace mathlib {
 
-    Vec3 operator+(const Vec3& other) const;
-    Vec3 operator-(const Vec3& other) const;
-    double operator*(const Vec3& other) const;
-};
-Vec3 vecSum(const Vec3& a, const Vec3& b);
-Vec3 vecScalar(float fac, const Vec3& v);
-double dot(const Vec3& a, const Vec3& b);
-Vec3 cross(const Vec3& a, const Vec3& b);
+  struct Vec3 {
+      double x, y, z;
 
-std::ostream& operator<<(std::ostream& os, const Vec3& v);
+      double length() const;
+      Vec3 normalize() const;
+      Vec3 operator+(const Vec3& other) const;
+      Vec3 operator-(const Vec3& other) const;
+      double operator*(const Vec3& other) const;
+  };
 
-#endif // MATHLIB_LIBRARY_H
+  Vec3 vecSum(const Vec3& a, const Vec3& b);
+  Vec3 vecScalar(float fac, const Vec3& v);
+  double dot(const Vec3& a, const Vec3& b);
+  Vec3 cross(const Vec3& a, const Vec3& b);
+  std::ostream& operator<<(std::ostream& os, const Vec3& v);
+
+}  
+#endif  // MATHLIB_LIBRARY_H_
--- sem2/PetrovMA/homework_1/mathexe/main.cpp
+++ sem2/PetrovMA/homework_1/mathexe/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+
 #include "libs/mathlib.h"
 
 int main() {
-    Vec3 v1 = { 1, 2, 3 };
-    Vec3 v2 = { 3, 2, 1 };
-    std::cout << v1 << " " << v2 << std::endl;
-    std::cout << v1 * v2 << std::endl;
-    std::cout << v1 + v2 << std::endl;
-    std::cout << v1 - v2 << std::endl;
-    std::cout << cross(v1, v2) << std::endl;
-    std::cout << v1.length() << std::endl;
-    std::cout << v1.normalize() << std::endl;
-    std::cout << v1.normalize().length() << std::endl;
-    return 0;
+  mathlib::Vec3 v1 = {1, 2, 3};
+  mathlib::Vec3 v2 = {3, 2, 1};
+  std::cout << v1 << " " << v2 << std::endl;
+  std::cout << v1 * v2 << std::endl;
+  std::cout << v1 + v2 << std::endl;
+  std::cout << v1 - v2 << std::endl;
+  std::cout << cross(v1, v2) << std::endl;
+  std::cout << v1.length() << std::endl;
+  std::cout << v1.normalize() << std::endl;
+  std::cout << v1.normalize().length() << std::endl;
+  return 0;
 }
