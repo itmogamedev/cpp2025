b8a1c7fc037b1302c9f119b1950ad25f33f9ebf9
IskatelNik <bombur07@gmail.com>
2026-03-25 14:04:31 +0300

    Update Vec3.h
    Изменение Vec3.h по C++ google style

--- sem2/MakarovNE/homework_1/Vec3.h
+++ sem2/MakarovNE/homework_1/Vec3.h
@@ -8,25 +8,23 @@
 
 #include <iostream>
 
-struct MATHVEC3_API Vec3
-{
-	float x_coord;
-	float y_coord;
-	float z_coord;
+struct MATHVEC3_API Vec3 {
+  float x_coord;
+  float y_coord;
+  float z_coord;
 
-	Vec3();
-	Vec3(float x, float y, float z);
+  Vec3();
+  Vec3(float x, float y, float z);
 
-	Vec3 operator+(const Vec3& other) const;
-	Vec3 operator-(const Vec3& other) const;
-	Vec3 operator*(float scalar) const;
-	friend MATHVEC3_API Vec3 operator*(float scalar, const Vec3& vec);
+  Vec3 operator+(const Vec3& other) const;
+  Vec3 operator-(const Vec3& other) const;
+  Vec3 operator*(float scalar) const;
+  friend MATHVEC3_API Vec3 operator*(float scalar, const Vec3& vec);
 
-	float skProduct(const Vec3& other) const;
-	Vec3 vcProduct(const Vec3& other) const;
-	float length() const;
-	Vec3 normalize() const;
+  float skProduct(const Vec3& other) const;
+  Vec3 vcProduct(const Vec3& other) const;
+  float length() const;
+  Vec3 normalize() const;
 };
 
-MATHVEC3_API std::ostream&
-operator<<(std::ostream& os, const Vec3& vec);
+MATHVEC3_API std::ostream& operator<<(std::ostream& os, const Vec3& vec);
