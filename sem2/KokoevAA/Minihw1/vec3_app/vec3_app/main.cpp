d44a78d4e6a3a357a2dcbc378aa97c1da795b48c
Kukarekun <pixel.bro.2017@bk.ru>
2026-03-25 22:35:10 +0300

    FIX
    Style fix

--- sem2/KokoevAA/Minihw1/vec3_app/vec3_app/main.cpp
+++ sem2/KokoevAA/Minihw1/vec3_app/vec3_app/main.cpp
@@ -3,18 +3,18 @@
 #include "Vec3.h"
 
 int main() {
-	Vec3 a(1.0, 2.0, 3.0);
-	Vec3 b(4.0, 5.0, 6.0);
+  Vec3 a(1.0, 2.0, 3.0);
+  Vec3 b(4.0, 5.0, 6.0);
 
-	std::cout << "a = " << a << "\n";
-	std::cout << "b = " << b << "\n";
-	std::cout << "a + b = " << a + b << "\n";
-	std::cout << "a - b = " << a - b << "\n";
-	std::cout << "a * 2 = " << a * 2.0 << "\n";
-	std::cout << "scalarMult(a, b) = " << a.scalarMultiply(b) << "\n";
-	std::cout << "vectMult(a, b) = " << a.vectorMultiply(b) << "\n";
-	std::cout << "|a| = " << a.length() << "\n";
-	std::cout << "normalize(a) = " << a.normalize() << "\n";
+  std::cout << "a = " << a << "\n";
+  std::cout << "b = " << b << "\n";
+  std::cout << "a + b = " << a + b << "\n";
+  std::cout << "a - b = " << a - b << "\n";
+  std::cout << "a * 2 = " << a * 2.0 << "\n";
+  std::cout << "scalarMult(a, b) = " << a.scalarMultiply(b) << "\n";
+  std::cout << "vectMult(a, b) = " << a.vectorMultiply(b) << "\n";
+  std::cout << "|a| = " << a.length() << "\n";
+  std::cout << "normalize(a) = " << a.normalize() << "\n";
 
-	return 0;
+  return 0;
 }
--- sem2/KokoevAA/Minihw1/vec3_library/vec3_library/Vec3.h
+++ sem2/KokoevAA/Minihw1/vec3_library/vec3_library/Vec3.h
@@ -10,27 +10,27 @@
 #endif
 
 class VEC3_API Vec3 {
-public:
-	double x;
-	double y;
-	double z;
+ public:
+  double x;
+  double y;
+  double z;
 
-	Vec3();
-	Vec3(double x_value, double y_value, double z_value);
+  Vec3();
+  Vec3(double x_value, double y_value, double z_value);
 
-	Vec3 add(const Vec3& other) const;
-	Vec3 subtract(const Vec3& other) const;
-	Vec3 multiplyByScalar(double scalar) const;
+  Vec3 add(const Vec3& other) const;
+  Vec3 subtract(const Vec3& other) const;
+  Vec3 multiplyByScalar(double scalar) const;
 
-	double scalarMultiply(const Vec3& other) const;
-	Vec3 vectorMultiply(const Vec3& other) const;
+  double scalarMultiply(const Vec3& other) const;
+  Vec3 vectorMultiply(const Vec3& other) const;
 
-	double length() const;
-	Vec3 normalize() const;
+  double length() const;
+  Vec3 normalize() const;
 
-	Vec3 operator+(const Vec3& other) const;
-	Vec3 operator-(const Vec3& other) const;
-	Vec3 operator*(double scalar) const;
+  Vec3 operator+(const Vec3& other) const;
+  Vec3 operator-(const Vec3& other) const;
+  Vec3 operator*(double scalar) const;
 };
 
 VEC3_API std::ostream& operator<<(std::ostream& out, const Vec3& vec);
