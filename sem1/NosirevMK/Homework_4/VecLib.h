fc99d406886770ba8c832ffc0c8ce6bf2cd2d1f1
MakariyNosierv <nosirevmakariy@gmail.com>
2026-03-26 20:09:39 +0300

    Homework 4.1
    Fixed style

--- sem1/NosirevMK/Homework_4/VecLib.h
+++ sem1/NosirevMK/Homework_4/VecLib.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 // Макрос для экспорта/импорта функций и классов из DLL
 #ifdef VECLIB_EXPORTS
@@ -10,26 +10,25 @@
 #define VECLIB_API __declspec(dllimport)
 #endif
 
-struct VECLIB_API Vec3
-{
-    float x, y, z;
+struct VECLIB_API Vec3 {
+  float x, y, z;
 
-    // Конструкторы
-    Vec3();
-    Vec3(float x, float y, float z);
+  // Конструкторы
+  Vec3();
+  Vec3(float x, float y, float z);
 
-    // Методы
-    float Length() const;
-    Vec3 Normalize() const;
+  // Методы
+  float Length() const;
+  Vec3 Normalize() const;
 
-    // Перегрузка операторов (члены класса)
-    Vec3 operator+(const Vec3& other) const;
-    Vec3 operator-(const Vec3& other) const;
-    Vec3 operator*(float scalar) const;
+  // Перегрузка операторов (члены класса)
+  Vec3 operator+(const Vec3& other) const;
+  Vec3 operator-(const Vec3& other) const;
+  Vec3 operator*(float scalar) const;
 
-    // Дружественные функции для скалярного умножения (scalar * vec) и вывода
-    friend VECLIB_API Vec3 operator*(float scalar, const Vec3& vec);
-    friend VECLIB_API std::ostream& operator<<(std::ostream& os, const Vec3& vec);
+  // Дружественные функции для скалярного умножения (scalar * vec) и вывода
+  friend VECLIB_API Vec3 operator*(float scalar, const Vec3& vec);
+  friend VECLIB_API std::ostream& operator<<(std::ostream& os, const Vec3& vec);
 };
 
 // Отдельные функции для скалярного и векторного произведения
