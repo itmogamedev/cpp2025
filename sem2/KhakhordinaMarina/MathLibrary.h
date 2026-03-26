2ae2df6898623c121b2035e1b2a5a6f157cfdc29
Marina <kharkhordinam@gmail.com>
2026-03-26 20:07:17 +0300

    miniHW1
    исправила пробелы в dllmain и MathLibrary.h

--- sem2/KhakhordinaMarina/MathLibrary.h
+++ sem2/KhakhordinaMarina/MathLibrary.h
@@ -10,18 +10,20 @@
 #include <cmath>
 
 struct MATHLIB_API Vec {
-	float x, y, z;
-	Vec(float x = 0, float y = 0, float z = 0);
+  float x, y, z;
+  
+  Vec(float x = 0, float y = 0, float z = 0);
+
 	// operators 
-	Vec operator+(const Vec& other) const;
-	Vec operator-(const Vec& other) const;
-	Vec operator*(float scalar) const; // na number
-	float operator*(const Vec& other) const; // scalar
+  Vec operator+(const Vec& other) const;
+  Vec operator-(const Vec& other) const;
+  Vec operator*(float scalar) const; // na number
+  float operator*(const Vec& other) const; // scalar
 
 	//metods 
-	float length() const;
-	Vec normalize() const;
-	Vec cross(const Vec& other) const; //vectr proiz
+  float length() const;
+  Vec normalize() const;
+  Vec cross(const Vec& other) const; //vectr proiz
 
-	friend MATHLIB_API std::ostream& operator<<(std::ostream& os, const Vec& v);
+  friend MATHLIB_API std::ostream& operator<<(std::ostream& os, const Vec& v);
 };
--- sem2/KhakhordinaMarina/dllmain.cpp
+++ sem2/KhakhordinaMarina/dllmain.cpp
@@ -6,13 +6,12 @@ BOOL APIENTRY DllMain( HMODULE hModule,
                        LPVOID lpReserved
                      )
 {
-    switch (ul_reason_for_call)
-    {
+  switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
-        break;
+      break;
     }
     return TRUE;
 }
