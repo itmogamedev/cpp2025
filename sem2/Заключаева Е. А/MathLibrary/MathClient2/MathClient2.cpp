ac0211fd5dc121be624bf5db633f6a117484ea2e
liza-127-zakluchaeva <elizaveta.zaklyuchaeva@yandex.com>
2026-03-27 23:25:57 +0300

    Новый файл библиотеки так как старые не работали с проектом

---
+++ sem2/Заключаева Е. А/MathLibrary/MathClient2/MathClient2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "Vectors.h"
+
+int main()
+{
+    vectors a(1.f, 2.f, 3.f);
+    vectors b(4.f, 5.f, 6.f);
+
+    vectors c = a + b;
+    std::cout << "Summary:" << c << std::endl;
+
+    std::cout << "x = " << c.get_x() << std::endl;
+    std::cout << "y = " << c.get_y() << std::endl;
+    std::cout << "z = " << c.get_z() << std::endl;
+
+    vectors d = a - b;
+    std::cout << "Subtraction:" << d << std::endl;
+
+    vectors e = 2 * a;
+    std::cout << "Product out of 2: " << e << std::endl;
+
+    float scalar = a.sc_prod(a, b);
+    std::cout << "Scalar product: " << scalar << std::endl;
+    return 0;
+}
---
+++ sem2/Заключаева Е. А/MathLibrary/MathLibrary2.0/Vectors.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+#include <tuple>
+#include <cmath>
+#include <iostream>
+
+class MATHLIBRARY_API vectors {
+private:
+    float x, y, z;
+
+public:
+    vectors(float x1, float y1, float z1);
+    float get_x();
+    float get_y();
+    float get_z();
+
+    std::tuple<float, float, float> summary(vectors& v1, vectors& v2);
+    std::tuple<float, float, float> sub(vectors& v1, vectors& v2);
+    std::tuple<float, float, float> prod(int num, vectors& v1);
+    float sc_prod(vectors& v1, vectors& v2);
+    std::tuple<float, float, float> vec_prod(vectors& v1, vectors& v2);
+    float lenght(vectors& v1);
+    std::tuple<float, float, float> normal(vectors& v1);
+
+    friend MATHLIBRARY_API vectors operator+(vectors& v1, vectors& v2);
+    friend MATHLIBRARY_API vectors operator-(vectors& v1, vectors& v2);
+    friend MATHLIBRARY_API vectors operator*(int num, vectors& v1);
+    friend MATHLIBRARY_API std::ostream& operator<<(std::ostream& os, vectors& v1);
+};
