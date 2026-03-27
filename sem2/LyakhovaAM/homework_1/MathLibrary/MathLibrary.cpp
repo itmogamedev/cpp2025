0a5afbceb188404f7727d3e1b6c02c3de80976be
Lyakhova-Anastasia <nastya.lyahova595@gmail.com>
2026-03-27 21:30:18 +0300

    Update MathLibrary.cpp

--- sem2/LyakhovaAM/homework_1/MathLibrary/MathLibrary.cpp
+++ sem2/LyakhovaAM/homework_1/MathLibrary/MathLibrary.cpp
@@ -5,33 +5,33 @@
 Vec3::Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
 
 Vec3 Vec3::addVec(Vec3 a) {
-	return Vec3(x + a.x, y + a.y, z + a.z);
+  return Vec3(x + a.x, y + a.y, z + a.z);
 }
 
 Vec3 Vec3::subVec(Vec3 a) {
-	return Vec3(x - a.x, y - a.y, z - a.z);
+  return Vec3(x - a.x, y - a.y, z - a.z);
 }
 
 Vec3 Vec3::multiplicVec(double scal) {
-	return Vec3(x * scal, y * scal, z * scal);
+  return Vec3(x * scal, y * scal, z * scal);
 }
 
 double Vec3::dotProd(Vec3 a) {
-	return (a.x * x + a.y * y + a.z * z);
+  return (a.x * x + a.y * y + a.z * z);
 }
 
 Vec3 Vec3::crossProd(Vec3 a) {
-	return Vec3(a.y * z - a.z * y, a.z * x - a.x * z, a.x * y - a.y * x);
+  return Vec3(a.y * z - a.z * y, a.z * x - a.x * z, a.x * y - a.y * x);
 }
 
 double Vec3::lenVec() {
-	return sqrt(x * x + y * y + z * z);
+  return sqrt(x * x + y * y + z * z);
 }
 
 Vec3 Vec3::normVec() {
-	double len = lenVec();
-	if (len) return Vec3(x / len, y / len, z / len);
-	return *this;
+  double len = lenVec();
+  if (len) return Vec3(x / len, y / len, z / len);
+  return *this;
 }
 
 Vec3 Vec3::operator+(const Vec3& a) { return addVec(a); }
@@ -40,6 +40,6 @@ double Vec3::operator*(const Vec3& a) { return dotProd(a); }
 Vec3 Vec3::operator*(const double& a) { return multiplicVec(a); }
 
 std::ostream& operator<<(std::ostream& os, const Vec3& a) {
-	std::cout << '(' << a.x << "; " << a.y << "; " << a.z << ')';
-	return os;
+  std::cout << '(' << a.x << "; " << a.y << "; " << a.z << ')';
+  return os;
 }
