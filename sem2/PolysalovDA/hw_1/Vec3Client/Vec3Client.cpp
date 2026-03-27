5dc72a00b3e56f2e0147f9a2314ad8bb453bddce
plff <109690528+p0sd7@users.noreply.github.com>
2026-03-27 17:18:23 +0300

    Fix googel stail Vec3Client.cpp

--- sem2/PolysalovDA/hw_1/Vec3Client/Vec3Client.cpp
+++ sem2/PolysalovDA/hw_1/Vec3Client/Vec3Client.cpp
@@ -1,17 +1,18 @@
-﻿#define VEC3DLL_IMPORT  // для потребителя
+#define VEC3DLL_IMPORT
 #include "vec3.h"
+
+#include <conio.h>   // для _getch()
 #include <iostream>
-#include <conio.h>  // для _getch()
 
 int main() {
-    Vec3 v(1.0f, 2.0f, 3.0f);
-    std::cout << "Vector: " << v << std::endl;
-    std::cout << "Length: " << v.length() << std::endl;
+  Vec3 v(1.0f, 2.0f, 3.0f);
+  std::cout << "Vector: " << v << std::endl;
+  std::cout << "Length: " << v.length() << std::endl;
 
-    Vec3 v2 = v.normalized();
-    std::cout << "Normalized: " << v2 << std::endl;
+  Vec3 v2 = v.normalized();
+  std::cout << "Normalized: " << v2 << std::endl;
 
-    std::cout << "\nНажмите любую клавишу для выхода...";
-    _getch();  // ждет нажатия любой клавиши
-    return 0;
+  std::cout << "\nНажмите любую клавишу для выхода...";
+  _getch();  // ждет нажатия любой клавиши
+  return 0;
 }
