5a25fd2e746fcaa656fbf2805d894de7bed71ab2
Lyakhova-Anastasia <nastya.lyahova595@gmail.com>
2026-03-27 22:05:55 +0300

    Update MathLibrary.h

--- sem2/LyakhovaAM/homework_1/MathLibrary/MathLibrary.h
+++ sem2/LyakhovaAM/homework_1/MathLibrary/MathLibrary.h
@@ -12,19 +12,19 @@
 extern "C" struct Vec3 {
   double x, y, z;
 
-	DECLSPEC Vec3(double x = 0, double y = 0, double z = 0);
-	DECLSPEC Vec3 addVec(Vec3 a);
-	DECLSPEC Vec3 subVec(Vec3 a);
+  DECLSPEC Vec3(double x = 0, double y = 0, double z = 0);
+  DECLSPEC Vec3 addVec(Vec3 a);
+  DECLSPEC Vec3 subVec(Vec3 a);
   DECLSPEC Vec3 multiplicVec(double scal);
-	DECLSPEC double dotProd(Vec3 a);
-	DECLSPEC Vec3 crossProd(Vec3 a);
-	DECLSPEC double lenVec();
-	DECLSPEC Vec3 normVec();
+  DECLSPEC double dotProd(Vec3 a);
+  DECLSPEC Vec3 crossProd(Vec3 a);
+  DECLSPEC double lenVec();
+  DECLSPEC Vec3 normVec();
 
-	DECLSPEC Vec3 operator+(const Vec3& a);
-	DECLSPEC Vec3 operator-(const Vec3& a);
-	DECLSPEC double operator*(const Vec3& a);
-	DECLSPEC Vec3 operator*(const double& a);
+  DECLSPEC Vec3 operator+(const Vec3& a);
+  DECLSPEC Vec3 operator-(const Vec3& a);
+  DECLSPEC double operator*(const Vec3& a);
+  DECLSPEC Vec3 operator*(const double& a);
 };
 
 extern "C" DECLSPEC std::ostream& operator<<(std::ostream& os, const Vec3& a);
