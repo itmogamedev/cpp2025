569d8e4163c6324a01ad4123561c072d1f21d780
Daniil Grishin <grishin.daniil.02@gmail.com>
2026-03-28 14:27:19 +0300

    Fixed some student routes
    Пофиксил несколько папок студентов, которые не читают ТЗ

---
+++ sem2/ShnaiderAR/Vec3Solution/TestApp/TestApp.cpp
@@ -0,0 +1,17 @@
+﻿#pragma comment(lib, "MathLibrary.lib")
+#include <iostream>
+
+#include "Vec3.h"
+
+int main() {
+  Vec3 v1(1, 2, 2);
+  Vec3 v2(4, 5, 6);
+
+  std::cout << "V1: " << v1 << " | Length: " << v1.length() << std::endl;
+  std::cout << "V1 Normalized: " << v1.normalize() << std::endl;
+  std::cout << "V1 + V2 = " << (v1 + v2) << std::endl;
+  std::cout << "Scalar product (V1 * V2): " << (v1 * v2) << std::endl;
+  std::cout << "Cross product: " << v1.cross(v2) << std::endl;
+
+  return 0;
+}
