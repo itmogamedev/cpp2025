7b7445e67d425129a0155144a237919c3fb690de
Dmitriy Girak <rodnayasem@gmail.com>
2026-04-23 21:28:15 +0300

    Update TypingGame.h

--- sem2/GirakDA/homework_2/TypingGame.h
+++ sem2/GirakDA/homework_2/TypingGame.h
@@ -5,28 +5,28 @@
 #include <string>
 
 class TypingGame {
-public:
-    TypingGame();
-    void run();
+ public:
+  TypingGame();
+  void run();
 
-private:
-    sf::RenderWindow window;
-    sf::Font font;
-    sf::Text title_text;
-    sf::Text char_text;
-    sf::Text score_text;
-    sf::Text timer_text;
-    sf::Text game_over_text;
-    sf::Text restart_text;
+ private:
+  sf::RenderWindow window;
+  sf::Font font;
+  sf::Text title_text;
+  sf::Text char_text;
+  sf::Text score_text;
+  sf::Text timer_text;
+  sf::Text game_over_text;
+  sf::Text restart_text;
 
-    char current_char;
-    int score;
-    float time_left;
-    bool game_active;
+  char current_char;
+  int score;
+  float time_left;
+  bool game_active;
 
-    void setupText();
-    void generateRandomChar();
-    void handleInput(char input);
-    void updateText();
-    void resetGame();
+  void setupText();
+  void generateRandomChar();
+  void handleInput(char input);
+  void updateText();
+  void resetGame();
 };
