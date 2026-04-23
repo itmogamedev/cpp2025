345b86099a98c8207630a7bc6d56bf673267a55f
Agego14 <4ekan357@mail.ru>
2026-04-23 21:56:58 +0300

    Chekantsev miniDZ 2 (fixed)

--- sem2/ChekantsevDV/SFML_game_OSU/SFML_game_OSU/Label.h
+++ sem2/ChekantsevDV/SFML_game_OSU/SFML_game_OSU/Label.h
@@ -7,13 +7,14 @@ using namespace sf;
 using namespace std;
 
 class Label : public Transformable, public Drawable {
-private:
-	Font font;
-	Text text;
-	void draw(RenderTarget& target, RenderStates states) const;
-public:
-	Label(string text, string font = "PressStart2P-Regular.ttf");
-	void setString(string text);
-	void centerString(Vector2u window);
-	Vector2f getSize();
+ private:
+  Font font;
+  Text text;
+  void draw(RenderTarget& target, RenderStates states) const;
+
+ public:
+  Label(string text, string font = "PressStart2P-Regular.ttf");
+  void setString(string text);
+  void centerString(Vector2u window);
+  Vector2f getSize();
 };
