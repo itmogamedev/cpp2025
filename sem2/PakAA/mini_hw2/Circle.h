bbf10c3ad710055efa13f101b441a63ee4069a8d
AndeWs <pak2007aa@gmail.com>
2026-04-24 16:30:23 +0300

    вроде исправил

--- sem2/PakAA/mini_hw2/Circle.h
+++ sem2/PakAA/mini_hw2/Circle.h
@@ -4,29 +4,29 @@
 #include <SFML/Graphics.hpp>
 
 class Circle {
-public:
-	Circle(float radius, float field_width, float field_height, float top_offset);
+ public:
+  Circle(float radius, float field_width, float field_height, float top_offset);
 
-	void update(float dt);
-	void draw(sf::RenderWindow& window);
-	bool contains(float mx, float my) const;
-	void respawn();
-	void setTexture(const sf::Texture* texture);
+  void update(float dt);
+  void draw(sf::RenderWindow& window);
+  bool contains(float mx, float my) const;
+  void respawn();
+  void setTexture(const sf::Texture* texture);
 
-private:
-	void randomize();
-	void applyTexture();
+ private:
+  void randomize();
+  void applyTexture();
 
-	float radius;
-	float field_width;
-	float field_height;
-	float top_offset;
-	float x;
-	float y;
-	float vx;
-	float vy;
-	sf::CircleShape shape;
-	const sf::Texture* texture;
+  float radius;
+  float field_width;
+  float field_height;
+  float top_offset;
+  float x;
+  float y;
+  float vx;
+  float vy;
+  sf::CircleShape shape;
+  const sf::Texture* texture;
 };
 
-#endif  
+#endif
