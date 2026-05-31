aab4b66775fcc71becbf58a9f39b72460df77797
Drozdova Alina <drozdov.a.c2@gmail.com>
2026-05-31 14:10:00 +0300

    Correction mini_hw_5

--- sem2/DrozdovaAA/mini_homework_5/Card.h
+++ sem2/DrozdovaAA/mini_homework_5/Card.h
@@ -2,11 +2,11 @@
 #include <SFML/Graphics.hpp>
 
 struct Card {
-	sf::RectangleShape shape; //обьект в виде прямоугольника
-	char symbol; //буква на карточке
-	bool is_face_up; //открыта/закрыта 
-	bool is_matched; //найдена ли пара
+  sf::RectangleShape shape;  // обьект в виде прямоугольника
+  char symbol;               // буква на карточке
+  bool is_face_up;           // открыта/закрыта
+  bool is_matched;           // найдена ли пара
 
-	Card(float x, float y, float width, float height, char sym);
-	void draw(sf::RenderWindow& window, const sf::Font& font) const;
+  Card(float x, float y, float width, float height, char sym);
+  void draw(sf::RenderWindow& window, const sf::Font& font) const;
 };
